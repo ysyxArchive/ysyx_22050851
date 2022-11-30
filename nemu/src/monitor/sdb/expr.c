2af0f9688b02600360274225ae8caf3a996e5eff
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-30 13:50:45 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:50:45 up  3:46,  3 users,  load average: 0.04, 0.01, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -113,6 +113,7 @@ static bool make_token(char* e) {
 
         tokens[nr_token].type = rules[i].token_type;
         strncpy(tokens[nr_token].str, substr_start, substr_len);
+        tokens[nr_token].str[substr_len] = '\0';
 
         position += substr_len;
 
