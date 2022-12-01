9816ce5785c51146aaf5af950ed54ceee39ee4f0
ysyx_22050851-ldz <dg_gua@foxmail.com>
2022-12-01 23:58:11 +0800

    impl watchpoint and corresponding instructions

--- nemu/include/sdb.h
+++ nemu/include/sdb.h
@@ -1,24 +1,39 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #ifndef __SDB_H__
 #define __SDB_H__
 
 #include <common.h>
 
-word_t expr(char *e, bool *success);
+word_t expr(char* e, bool* success);
 void test_expr();
 
+/**
+ * @return watchpoint id if success, -1 if error and print error info
+ */
+int new_wp(char* expr);
+
+bool free_wp(int NO);
+
+void list_wp();
+
+char* get_wp_expr(int NO);
+
+/**
+ * @return wp.NO if watchpoint triggered, else return -1;
+ */
+int check_watchpoint();
 #endif
