904cfcdd45caf1ecd942db7a67a2947b95dca3b9
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-04 21:15:45 +0800

    > sim RTL ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:15:45 up  6:06,  3 users,  load average: 0.05, 0.03, 0.00

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,7 +1,11 @@
 module top(
-  input a,
-  input b,
-  input s,
-  output y);
-  assign y = (a & ~s) | (b & s);
+  input a[3:0],
+  input s[1:0],
+  output reg y);
+  always @ (s or a)
+    case (s)
+        0: y = a[0];
+        1: y = a[1];
+        2: y = a[2];
+        3: y = a[3];
 endmodule
