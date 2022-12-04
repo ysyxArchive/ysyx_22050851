b04a6b789e5814757d7c41051e45283238ed0e53
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-04 15:30:06 +0800

    > sim RTL ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:30:06 up 21 min,  0 users,  load average: 0.38, 0.38, 0.59

---
+++ npc/csrc/main.cpp
@@ -0,0 +1,35 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vtop.h"
+int main(int argc, char** argv) {
+  int i = 0;
+  VerilatedContext* contextp = new VerilatedContext;
+
+  contextp->commandArgs(argc, argv);
+  VerilatedVcdC* tfp = new VerilatedVcdC();
+  contextp->traceEverOn(true);
+  Vtop* top = new Vtop{contextp};
+  top->trace(tfp, 0);
+  tfp->open("wave.vcd");
+  printf("evaling\b\n");
+  while (i++ <= 20) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+
+    printf("a = %d, b = %d, f = %d\n", a, b, top->c);
+
+    tfp->dump(contextp->time());
+    contextp->timeInc(1);
+
+    assert(top->c == (a ^ b));
+  }
+  tfp->close();
+
+  return 0;
+}
---
+++ npc/vsrc/top.v
@@ -0,0 +1,6 @@
+module top(
+  input a,
+  input b,
+  output c);
+  assign c = a ^ b;
+endmodule
