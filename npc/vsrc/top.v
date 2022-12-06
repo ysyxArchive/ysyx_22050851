aff56d48435948453feaa8d604a7e6d4fbc5e51b
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-06 18:38:24 +0800

    > sim RTL ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:38:24 up  4:25,  1 user,  load average: 0.00, 0.01, 0.07

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -5,8 +5,8 @@ module top(
   output [3:0] out);
 
 counter mycounter1(.rst(rst), .clk(clk), .out(out[3]));
-counter mycounter2(.rst(rst), .clk(out[3]), .out(out[2]));
-counter mycounter3(.rst(rst), .clk(out[2]), .out(out[1]));
-counter mycounter4(.rst(rst), .clk(out[1]), .out(out[0]));
+counter mycounter2(.rst(rst), .clk(~out[3]), .out(out[2]));
+counter mycounter3(.rst(rst), .clk(~out[2]), .out(out[1]));
+counter mycounter4(.rst(rst), .clk(~out[1]), .out(out[0]));
 endmodule
 
