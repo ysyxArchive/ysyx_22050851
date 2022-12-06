a25256baf0471a9cff013831e70a9a99a78f48d6
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-06 18:33:45 +0800

    > sim RTL ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:33:45 up  4:21,  1 user,  load average: 0.01, 0.04, 0.10

--- npc/vsrc/counter.v
+++ npc/vsrc/counter.v
@@ -4,11 +4,11 @@ module counter(
     output reg out);
 
     initial begin
-        out = 0;
+        out <= 0;
     end
 
     always @(posedge rst)
-        out = 0;
+        out <= 0;
 
 
     always @(posedge clk)
