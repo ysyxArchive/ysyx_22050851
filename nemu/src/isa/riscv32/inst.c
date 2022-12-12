61b3d162edde1cf6fb514b2b748c3eec3e70fbcb
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-12 22:59:13 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:59:13 up 14:29,  1 user,  load average: 0.14, 0.05, 0.01

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -45,6 +45,7 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_CI:                  immCI();break;
   }
+  
 }
 
 static int decode_exec(Decode *s) {
