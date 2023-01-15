ae101380d70561dbdcc4fa3b3cbd0641ebcb29c3
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-15 18:56:52 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:56:52 up  2:15,  1 user,  load average: 0.15, 0.06, 0.08

--- nemu/README.md
+++ nemu/README.md
@@ -3,7 +3,7 @@
 NEMU(NJU Emulator) is a simple but complete full-system emulator designed for teaching purpose.
 Currently it supports x86, mips32, riscv32 and riscv64.
 To build programs run above NEMU, refer to the [AM project](https://github.com/NJU-ProjectN/abstract-machine).
-
+`
 The main features of NEMU include
 * a small monitor with a simple debugger
   * single step
--- npc/playground/src/InstructionDecodeUnit.scala
+++ npc/playground/src/InstructionDecodeUnit.scala
@@ -6,110 +6,117 @@ import chisel3.util.Enum
 
 import scala.language.postfixOps
 
-object Source {
-  val default = apply()
-
-
-  def apply(value: UInt = 0.U, isReg: Bool = false.B) = {
-    val f = new Source()
-    f.value := value
-    f.isReg := isReg
-    f
-  }
-}
-
 class Source() extends Bundle {
   val value = UInt(64.W)
   val isReg = Bool()
 }
-
-object OperationType extends ChiselEnum {
-  val add, noMatch = Value
-}
-
-object Operation {
-
-  val default = new Operation(Source.default, Source.default, Source.default, OperationType.noMatch.asUInt)
-
-  def apply(s1: Source, s2: Source, dst: Source, opType: OperationType.Type) = new Operation(s1, s2, dst, opType.asUInt)
-
-  def apply() = new Operation(Source(), Source(), Source())
-
-}
-
-class Operation(val src1: Source, val src2: Source, val dst: Source, val opType: UInt = UInt(2.W)) extends Bundle {}
-
-object InstructionType extends ChiselEnum {
-  val rType, iType, sType, uType, noType = Value
-}
-
-object InstructionResType extends ChiselEnum {
-  val further, noMatch, ok, other = Value
-}
-
-object Instruction {
+object Source {
+  val default = RegInit(apply())
 
 
-  val further = new Instruction(InstructionResType.further.asUInt, InstructionType.noType.asUInt, Operation.default)
-  val noMatch = new Instruction(InstructionResType.noMatch.asUInt, InstructionType.noType.asUInt, Operation.default)
-
-  def apply() = new Instruction()
-
-
-  def apply(instType: InstructionType.Type, op: Operation) = new Instruction(InstructionResType.ok.asUInt, instType.asUInt, op)
-}
-
-class Instruction(val status: UInt = UInt(3.W), val instructionType: UInt = UInt(3.W), val op: Operation = Operation()) extends Bundle {
-  /** no match Instruction */
-
-}
-
-object Utils {
-  def signalExtend(num: UInt, length: Int): UInt = {
-    Cat(Fill(64 - length, num(length - 1, length - 1)), num)
+  def apply(value: UInt, isReg: Bool) = {
+    val f = new Source()
+    f.value := value
+    f.isReg := isRegs
+    f
   }
 }
 
 
-class InstructionDecodeUnit extends Module {
-  val io = IO(new Bundle {
-    val enable = Input(Bool())
-    val inst = Input(UInt(32.W))
-
-  })
-  val output = IO(Decoupled(Instruction()))
-  val rs1 = io.inst(19, 15)
-  val rs2 = io.inst(24, 20)
-  val rd = io.inst(11, 7)
-  val opcode = io.inst(6, 0)
-  val funct3 = io.inst(14, 12)
-  val immI = io.inst(31, 20)
-  val immS = Cat(io.inst(31, 25), io.inst(11, 7))
-  val test = Mux(opcode === 1.U, Source.default, Source.default)
-  val result = MuxLookup(opcode, Instruction.further, Seq("b0010011".U -> Instruction.further))
-  //    when(result.status === Instruction.further) {
-  val result2 = MuxLookup(Cat(funct3, opcode), Instruction(), Seq(
-    "b0000010011".U -> Instruction(
-      InstructionType.iType, Operation(
-        Source(rs1, true.B),
-        Source(Utils.signalExtend(immI, 12), false.B),
-        Source(rd, true.B),
-        OperationType.add
-      )
-
-    )
-  ))
-
-  val finalresult = MuxLookup(result.status.asUInt, result, Seq(
-    InstructionResType.ok.asUInt -> result,
-    InstructionResType.further.asUInt -> MuxLookup(result2.status.asUInt, result2, Seq(
-      InstructionResType.ok.asUInt -> result2,
-    ))
-  ))
-
-  when(io.enable) {
-    output.bits := finalresult
-    output.valid := true.B
-  }
-
-}
+//object OperationType extends ChiselEnum {
+//  val add, noMatch = Value
+//}
+//
+//object Operation {
+//
+//  val default = new Operation(Source.default, Source.default, Source.default, OperationType.noMatch.asUInt)
+//
+//  def apply(s1: Source, s2: Source, dst: Source, opType: OperationType.Type) = new Operation(s1, s2, dst, opType.asUInt)
+//
+//  def apply() = new Operation(Source(), Source(), Source())
+//
+//}
+//
+////class Operation(val src1: Source, val src2: Source, val dst: Source, val opType: UInt = UInt(2.W)) extends Bundle {}
+////class Operation() extends Bundle {
+////  val src1 =  Source, val src2: Source, val dst: Source, val opType: UInt = UInt(2.W)
+////}
+//
+//object InstructionType extends ChiselEnum {
+//  val rType, iType, sType, uType, noType = Value
+//}
+//
+//object InstructionResType extends ChiselEnum {
+//  val further, noMatch, ok, other = Value
+//}
+//
+//class Instruction() extends Bundle {
+//  val status = UInt(3.W)
+//  val instructionType = UInt(3.W)
+//  val op = Operation()
+//}
+//object Instructin {
+//
+//
+//  def further = {
+//    var inst = new Instruction
+////    inst. InstructionResType.further.asUInt, InstructionType.noType.asUInt, Operation.default
+//  }
+//
+//  val noMatch = new Instruction(InstructionResType.noMatch.asUInt, InstructionType.noType.asUInt, Operation.default)
+//
+//  def apply() = new Instruction
+//
+//
+//}
+//
+//
+//object Utils {
+//  def signalExtend(num: UInt, length: Int): UInt = {
+//    Cat(Fill(64 - length, num(length - 1, length - 1)), num)
+//  }
+//}
+//
+//
+//class InstructionDecodeUnit extends Module {
+//  val io = IO(new Bundle {
+//    val enable = Input(Bool())
+//    val inst = Input(UInt(32.W))
+//
+//  })
+//  val output = IO(Decoupled(Instruction()))
+//  val rs1 = io.inst(19, 15)
+//  val rs2 = io.inst(24, 20)
+//  val rd = io.inst(11, 7)
+//  val opcode = io.inst(6, 0)
+//  val funct3 = io.inst(14, 12)
+//  val immI = io.inst(31, 20)
+//  val immS = Cat(io.inst(31, 25), io.inst(11, 7))
+//  val test = Mux(opcode === 1.U, Source.default, Source.default)
+//  val result = MuxLookup(opcode, Instruction.further, Seq("b0010011".U -> Instruction.further))
+//  //    when(result.status === Instruction.further) {
+//  val result2 = MuxLookup(Cat(funct3, opcode), Instruction(), Seq(
+//    "b0000010011".U -> Instruction(
+//      InstructionType.iType, Operation(
+//        Source(rs1, true.B),
+//        Source(Utils.signalExtend(immI, 12), false.B),
+//        Source(rd, true.B),
+//        OperationType.add
+//      )
+//
+//    )
+//  ))
+//
+//  val finalresult = MuxLookup(result.status.asUInt, result, Seq(
+//    InstructionResType.ok.asUInt -> result,
+//    InstructionResType.further.asUInt -> MuxLookup(result2.status.asUInt, result2, Seq(
+//      InstructionResType.ok.asUInt -> result2,
+//    ))
+//  ))
+//
+//  when(io.enable) {
+//    output.bits := finalresult
+//    output.valid := true.B
+//  }
+//
+//}
