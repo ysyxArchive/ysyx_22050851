b0195e43d4c299ebfc507230be9fdf26f0f786a5
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-15 19:34:54 +0800

    > generate verilog ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:34:54 up  2:53,  1 user,  load average: 0.45, 0.35, 0.24

--- npc/playground/src/InstructionDecodeUnit.scala
+++ npc/playground/src/InstructionDecodeUnit.scala
@@ -32,7 +32,7 @@ object Operation {
   val default = Operation(Source.default, Source.default, Source.default, OperationType.noMatch)
 
   def apply(s1: Source, s2: Source, dst: Source, opType: OperationType.Type) = {
-    val op = new Operation()
+    val op = Wire(Operation())
     op.src1   := s1
     op.src2   := s2
     op.dst    := dst
