bb44072d2e93d74ed37d5c1fc613a09badcf6fad
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 00:38:01 +0800

    > generate verilog ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:38:01 up  7:56,  1 user,  load average: 0.02, 0.08, 0.06

--- npc/playground/src/InstructionExecuteUnit.scala
+++ npc/playground/src/InstructionExecuteUnit.scala
@@ -3,14 +3,14 @@ import chisel3._
 import chisel3.util._
 
 class InstructionExecuteUnit extends Module {
-  val in        = Flipped(Decoupled(Operation()))
-  val regIO     = Flipped(new RegisterFileIO())
-  val operation = RegInit(Operation, in.bits)
-  val src1      = operation.src1
-  val src2      = operation.src2
+  val in    = Flipped(Decoupled(Operation()))
+  val regIO = Flipped(new RegisterFileIO())
 
-  regIO.raddr1 := Mux(src1.isReg === true.B, src1.value, 0.U(64.W))
-  regIO.raddr2 := Mux(src2.isReg === true.B, in.bits.src2.value, 0.U(64.W))
+  val op = Wire(Operation)
+  op := in.bits
+
+  regIO.raddr1 := Mux(op.src1.isReg === true.B, src1.value, 0.U(64.W))
+  regIO.raddr2 := Mux(op.src1.isReg === true.B, in.bits.src2.value, 0.U(64.W))
 
   val src1val = Mux(in.bits.src1.isReg, regIO.rdata1, in.bits.src1.value)
   val src2val = Mux(in.bits.src1.isReg, regIO.rdata2, in.bits.src2.value)
