ad83d639c48ed31894ba63e1f792d0f0f245b5a0
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 00:39:45 +0800

    > generate verilog ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:39:45 up  7:57,  1 user,  load average: 0.38, 0.22, 0.11

--- npc/playground/src/InstructionExecuteUnit.scala
+++ npc/playground/src/InstructionExecuteUnit.scala
@@ -6,7 +6,7 @@ class InstructionExecuteUnit extends Module {
   val in    = Flipped(Decoupled(Operation()))
   val regIO = Flipped(new RegisterFileIO())
 
-  val op = Wire(Operation())
+  val op = input.deq()
   op := in.bits
 
   regIO.raddr1 := Mux(op.src1.isReg === true.B, in.bits.src1.value, 0.U(64.W))
