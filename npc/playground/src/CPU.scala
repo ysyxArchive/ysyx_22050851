12adfa130c45aaa368a33f47135d8c6de082d9b1
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 01:40:18 +0800

    > generate verilog ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:40:18 up 26 min,  1 user,  load average: 0.08, 0.14, 0.43

--- npc/playground/src/CPU.scala
+++ npc/playground/src/CPU.scala
@@ -2,11 +2,10 @@ import chisel3._
 import chisel3.util.Enum
 
 /**
- * Compute GCD using subtraction method.
- * Subtracts the smaller from the larger until register y is zero.
- * value in register x is then the GCD
- */
-
+  * Compute GCD using subtraction method.
+  * Subtracts the smaller from the larger until register y is zero.
+  * value in register x is then the GCD
+  */
 
 class CPU extends Module {
   val io = IO(new Bundle {
@@ -15,13 +14,14 @@ class CPU extends Module {
   })
 
   val regs = Module(new RegisterFile);
-  val pc = RegInit("h80000000".asUInt(64.W))
+  val pc   = RegInit("h80000000".asUInt(64.W))
 
   val state_fetch :: state_decode :: state_execute :: state_idle = Enum(3)
-  val cpuState = RegInit(state_fetch)
+  val cpuState                                                   = RegInit(state_fetch)
 
   val decoder = Module(new InstructionDecodeUnit)
-  val exe = Module(new InstructionExecuteUnit)
+  val exe     = Module(new InstructionExecuteUnit)
 
+  exe.op <> decoder.output
 
 }
