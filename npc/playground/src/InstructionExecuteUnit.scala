4926d6fc0192cca9942f9c7e9bc6576f34641c51
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 01:41:17 +0800

    > generate verilog ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:41:17 up 27 min,  1 user,  load average: 0.17, 0.17, 0.42

--- npc/playground/src/InstructionExecuteUnit.scala
+++ npc/playground/src/InstructionExecuteUnit.scala
@@ -6,24 +6,26 @@ class InstructionExecuteUnit extends Module {
   val in    = Flipped(Decoupled(Operation()))
   val regIO = Flipped(new RegisterFileIO())
 
-  val op = Wire(Operation())
-  op := in.bits
+  when(in.valid) {
+    val op = Wire(Operation())
+    op := in.bits
 
-  regIO.raddr1 := Mux(op.src1.isReg === true.B, in.bits.src1.value, 0.U(64.W))
-  regIO.raddr2 := Mux(op.src1.isReg === true.B, in.bits.src2.value, 0.U(64.W))
+    regIO.raddr1 := Mux(op.src1.isReg === true.B, in.bits.src1.value, 0.U(64.W))
+    regIO.raddr2 := Mux(op.src1.isReg === true.B, in.bits.src2.value, 0.U(64.W))
 
-  val src1val = Mux(in.bits.src1.isReg, regIO.rdata1, in.bits.src1.value)
-  val src2val = Mux(in.bits.src1.isReg, regIO.rdata2, in.bits.src2.value)
+    val src1val = Mux(in.bits.src1.isReg, regIO.rdata1, in.bits.src1.value)
+    val src2val = Mux(in.bits.src1.isReg, regIO.rdata2, in.bits.src2.value)
 
-  val ans = MuxLookup(
-    in.bits.opType.asUInt,
-    0.U,
-    Seq(
-      OperationType.add.asUInt -> (src1val + src2val)
+    val ans = MuxLookup(
+      in.bits.opType.asUInt,
+      0.U,
+      Seq(
+        OperationType.add.asUInt -> (src1val + src2val)
+      )
     )
-  )
-  regIO.waddr := regIO.wen
-  regIO.wdata := ans
-  regIO.wen   := RegNext(false.B, true.B)
+    regIO.waddr := regIO.wen
+    regIO.wdata := ans
+    regIO.wen   := RegNext(false.B, true.B)
+  }
 
 }
