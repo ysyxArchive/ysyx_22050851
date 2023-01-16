9175af87084a51e8aa45bbeadc477381c0df068f
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 20:54:31 +0800

    > sim RTL ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:54:31 up 13:45,  1 user,  load average: 0.18, 0.16, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -32,6 +32,7 @@ int main(int argc, char** argv) {
   tfp->open("wave.vcd");  // 打开vcd
   int time = 0;
   while (time < 100 && top->pcio_pc <= 0x80000000 + 6 * 4) {
+    printf("%x\n", (top->pcio_pc - 0x80000000) / 4);
     top->pcio_inst = mem[(top->pcio_pc - 0x80000000) / 4];
     uint64_t pc = top->pcio_pc;
 
