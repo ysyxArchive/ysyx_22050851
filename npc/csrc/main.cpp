d982418666ab9dbe71a66a640c4b2a8b9c5a5ac6
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 15:42:02 +0800

    > sim RTL ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:42:02 up  8:33,  1 user,  load average: 0.11, 0.08, 0.02

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,13 +1,22 @@
 #include "VCPU.h"
 #include "verilated.h"
+#include "verilated_vcd_c.h"
 int main(int argc, char** argv) {
   VerilatedContext* contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
+
+  Verilated::traceEverOn(true);  // 导出vcd波形需要加此语句
+  VerilatedVcdC* tfp = new VerilatedVcdC();  // 导出vcd波形需要加此语句
+
   VCPU* top = new VCPU{contextp};
+  top->trace(tfp, 0);
+  tfp->open("wave.vcd");  // 打开vcd
+
   while (!contextp->gotFinish()) {
     top->eval();
   }
   delete top;
   delete contextp;
+  delete tfp;
   return 0;
 }
