8b9890b0f1fd35c9c29a2f64517499e796c4b06f
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 14:08:58 +0800

    > generate verilog ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:08:58 up  7:00,  1 user,  load average: 0.35, 0.15, 0.09

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -6,6 +6,7 @@ object Elaborate extends App {
   val useMFC = true // use MLIR-based firrtl compiler
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
   println(args)
+  println("123")
   if (useMFC) {
     (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
   } else {
