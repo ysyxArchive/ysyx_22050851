0203ab830ca206cfa6b1fe651c557a08f1f3bae0
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-17 14:36:39 +0800

    > generate verilog ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:36:39 up 37 min,  1 user,  load average: 0.22, 0.12, 0.18

--- npc/playground/src/RegisterFile.scala
+++ npc/playground/src/RegisterFile.scala
@@ -23,10 +23,10 @@ class RegisterFileIO extends Bundle {
 class RegisterFile extends Module {
   val io   = IO(new RegisterFileIO())
   val regs = RegInit(VecInit(Seq.fill(32)(0.U(64.W))))
-  val pc   = RegInit("h80000000".asUInt(64.W))
+  val pc: UInt = RegNext(pc + 4.U, "h80000000".asUInt(64.W))
 
   io.pc := pc
- 
+
   when(io.wen && io.waddr =/= 0.U) {
     regs(io.waddr) := io.wdata
   }
