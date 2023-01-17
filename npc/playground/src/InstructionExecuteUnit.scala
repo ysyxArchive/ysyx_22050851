be0240eb06678eccfaab23a7da2e2883db4551b1
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-17 17:59:48 +0800

    > sim RTL ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:59:48 up  3:23,  1 user,  load average: 0.17, 0.28, 0.31

--- npc/playground/src/InstructionExecuteUnit.scala
+++ npc/playground/src/InstructionExecuteUnit.scala
@@ -5,19 +5,16 @@ import chisel3.util._
 class InstructionExecuteUnit extends Module {
   val in    = IO(Flipped(Decoupled(Operation())))
   val regIO = IO(Flipped(new RegisterFileIO()))
+  val op    = in.bits
 
   val inReady = RegInit(true.B)
   in.ready := inReady
 
   val writeEnable = RegInit(true.B)
   val writeAddr   = RegInit(0.U(5.W))
-  val writeData   = RegInit(0.U(64.W))
   regIO.wen   := writeEnable
-  regIO.waddr := writeAddr
-  regIO.wdata := writeData
-  writeAddr   := regIO.wen
+  regIO.waddr := op.dst.value
 
-  val op = in.bits
   regIO.raddr1 := Mux(op.src1.isReg, op.src1.value(4, 0), 0.U(5.W))
   regIO.raddr2 := Mux(op.src2.isReg, op.src2.value(4, 0), 0.U(5.W))
 
@@ -31,6 +28,6 @@ class InstructionExecuteUnit extends Module {
       OperationType.add.asUInt -> (src1val + src2val)
     )
   )
-  writeData := ans
+  regIO.wdata := ans
 
 }
