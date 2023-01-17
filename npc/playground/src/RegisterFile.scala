d6a8b33bcca43dca1ae34371691ea5e4f23aca90
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-17 15:00:16 +0800

    > sim RTL ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:00:16 up  1:01,  1 user,  load average: 0.69, 0.94, 0.79

--- npc/playground/src/RegisterFile.scala
+++ npc/playground/src/RegisterFile.scala
@@ -20,14 +20,14 @@ class RegisterFileIO extends Bundle {
   val out2   = Output(UInt(64.W))
   val raddr2 = Input(UInt(5.W))
 
-  val pc     = Output(UInt(64.W))
+  val pc = Output(UInt(64.W))
 }
 
 class RegisterFile extends Module {
   val io   = IO(new RegisterFileIO())
   val regs = RegInit(VecInit(Seq.fill(32)(0.U(64.W))))
-  val pc   = Wire(UInt(64.W))
-  pc := RegNext(pc + 4.U, "h80000000".asUInt(64.W))
+  val pc   = "h80000000".asUInt(64.W)
+  pc := RegNext(pc + 4.U)
 
   when(io.wen && io.waddr =/= 0.U) {
     regs(io.waddr) := io.wdata
