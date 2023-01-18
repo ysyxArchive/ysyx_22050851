5de17d45a8beb7d4fcc8a9c3c16e467d2fae4f14
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-19 00:14:15 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:14:15 up  2:04,  1 user,  load average: 0.09, 0.05, 0.01

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -25,15 +25,11 @@ $(BINARY): compile_git
 # Some convenient rules
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
-override ARGS += $(ARGS_DIFF) -b
+override ARGS += $(ARGS_DIFF)
 
-# ifeq ($(MAKECMDGOALS), batch)
-# $(info  $(MAKECMDGOALS))
-# $(info  $(ARGS))
-# endif
-# ifeq ($@, $@)
-# @echo $@
-# endif
+ifeq ($(MAKECMDGOALS), batch)
+override ARGS += -b
+endif
 # Command to execute NEMU
 IMG ?=
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
