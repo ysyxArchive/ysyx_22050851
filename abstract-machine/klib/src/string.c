67c7082e682508b8c6d9a03ef2c7c7f3ab12f537
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-20 02:24:56 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:24:56 up 11:36,  1 user,  load average: 0.11, 0.08, 0.02

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,46 +1,50 @@
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
+size_t strlen(const char* s) {
   panic("Not implemented");
 }
 
-char *strcpy(char *dst, const char *src) {
+char* strcpy(char* dst, const char* src) {
   panic("Not implemented");
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
+char* strncpy(char* dst, const char* src, size_t n) {
   panic("Not implemented");
 }
 
-char *strcat(char *dst, const char *src) {
+char* strcat(char* dst, const char* src) {
   panic("Not implemented");
 }
 
-int strcmp(const char *s1, const char *s2) {
+int strcmp(const char* s1, const char* s2) {
+  int i = 0;
+  while (s1[i] && s2[i] && s1[i] == s2[i]) {
+    i++;
+  }
+  return s1[i] - s2[i];
+}
+
+int strncmp(const char* s1, const char* s2, size_t n) {
   panic("Not implemented");
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
+void* memset(void* s, int c, size_t n) {
   panic("Not implemented");
 }
 
-void *memset(void *s, int c, size_t n) {
+void* memmove(void* dst, const void* src, size_t n) {
   panic("Not implemented");
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
+void* memcpy(void* out, const void* in, size_t n) {
   panic("Not implemented");
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
-}
-
-int memcmp(const void *s1, const void *s2, size_t n) {
+int memcmp(const void* s1, const void* s2, size_t n) {
   panic("Not implemented");
 }
 
