5a07e8d9c672fdcea1d9e6b83089f4689551d4f5
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-20 01:57:39 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:57:39 up 11:09,  1 user,  load average: 0.24, 0.17, 0.06

--- nemu/src/isa/riscv64/local-include/reg.h
+++ nemu/src/isa/riscv64/local-include/reg.h
@@ -1,17 +1,17 @@
 /***************************************************************************************
- * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
- *
- * NEMU is licensed under Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan
- *PSL v2. You may obtain a copy of Mulan PSL v2 at:
- *          http://license.coscl.org.cn/MulanPSL2
- *
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
- *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
- *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
- *
- * See the Mulan PSL v2 for more details.
- ***************************************************************************************/
+* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
 
 #ifndef __RISCV64_REG_H__
 #define __RISCV64_REG_H__
@@ -19,12 +19,7 @@
 #include <common.h>
 
 static inline int check_reg_idx(int idx) {
-  IFDEF(
-      CONFIG_RT_CHECK,
-      printf("%d", idx);
-      Assert(idx >= 0 && idx < 32,
-             "Assertion failed for checking reg index %d is between 0 and 32\n",
-             idx));
+  IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < 32));
   return idx;
 }
 
