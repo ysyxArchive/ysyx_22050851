7cf3feaad735af4358a2e8f5e3879b4611f67feb
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-20 00:01:56 +0800

    > generate verilog ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:01:56 up  9:13,  1 user,  load average: 1.04, 0.55, 0.25

--- npc/playground/src/InstructionExecuteUnit.scala
+++ npc/playground/src/InstructionExecuteUnit.scala
@@ -10,6 +10,9 @@ class InstructionExecuteUnit extends Module {
   val inReady = RegInit(true.B)
   in.ready := inReady
 
+  val blackBox = Module(new BlackBoxAdd);
+  blackBox.io.halt := false.B
+
   regIO.waddr := op.dst.value(4, 0)
 
   regIO.raddr1 := Mux(op.src1.stype === SourceType.reg.asUInt, op.src1.value(4, 0), 0.U(5.W))
@@ -45,7 +48,8 @@ class InstructionExecuteUnit extends Module {
     )
   )
 
-  regIO.wdata    := ans
-  regIO.regWrite := op.dst.stype === SourceType.reg.asUInt
-  regIO.pcWrite  := op.dst.stype === SourceType.pc.asUInt
+  blackBox.io.halt := in.bits.opType === OperationType.halt.asUInt
+  regIO.wdata      := ans
+  regIO.regWrite   := op.dst.stype === SourceType.reg.asUInt
+  regIO.pcWrite    := op.dst.stype === SourceType.pc.asUInt
 }
