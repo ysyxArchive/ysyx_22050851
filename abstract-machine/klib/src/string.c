bed73c0c84e7d95b3f9e202dee01b8611ba4ffe5
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-20 03:59:07 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:59:07 up 13:10,  1 user,  load average: 0.16, 0.06, 0.01

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,7 +9,13 @@ size_t strlen(const char* s) {
 }
 
 char* strcpy(char* dst, const char* src) {
-  panic("Not implemented");
+  int i = 0;
+  while (src[i]) {
+    dst[i] = src[i];
+    i++;
+  }
+  dst[i] = src[i];
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
@@ -17,7 +23,16 @@ char* strncpy(char* dst, const char* src, size_t n) {
 }
 
 char* strcat(char* dst, const char* src) {
-  panic("Not implemented");
+  int i = 0;
+  while (dst[i])
+    i++;
+  int j = 0;
+  while (src[j]) {
+    dst[i + j] = src[j];
+    j++;
+  }
+  dst[i + j] = 0;
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
@@ -33,7 +48,11 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 }
 
 void* memset(void* s, int c, size_t n) {
-  panic("Not implemented");
+  char* p = s;
+  for (int i = 0; i < n; i++) {
+    p[i] = (char)c;
+  }
+  return s;
 }
 
 void* memmove(void* dst, const void* src, size_t n) {
@@ -45,7 +64,14 @@ void* memcpy(void* out, const void* in, size_t n) {
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
-  panic("Not implemented");
+  const char* p1 = s1;
+  const char* p2 = s2;
+  char ret = 0;
+  int i = 0;
+  while (i < n && ret == 0) {
+    ret = p1[i] - p2[i];
+  }
+  return ret;
 }
 
 #endif
