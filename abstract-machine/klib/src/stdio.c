5e6680079d87cf492e4dff89b2031798f8e8a948
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-21 00:31:57 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:31:57 up  9:25,  2 users,  load average: 0.11, 0.10, 0.03

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,27 +1,78 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
+int num2str(char* out, int num) {
+  char bufs[30];
+  int outp = 0;
+  int bufp = 0;
+  if (num == 0) {
+    out[outp++] = '0';
+  } else {
+    if (num < 0) {
+      out[outp++] = '-';
+      num = -num;
+    }
+    while (num != 0) {
+      bufs[bufp++] = '0' + num % 10;
+      num = num / 10;
+    }
+    bufp--;
+    while (bufp >= 0) {
+      out[outp++] = bufs[bufp--];
+    }
+  }
+  out[outp] = 0;
+  return outp;
+}
+
+int printf(const char* fmt, ...) {
   panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
+int vsprintf(char* out, const char* fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
+int sprintf(char* out, const char* fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int outp = 0;
+  int fmtp = 0;
+  int offset = 0;
+  while (fmt[fmtp]) {
+    while (fmt[fmtp] != '%') {
+      out[outp++] = fmt[fmtp++];
+    }
+    fmtp++;
+    switch (fmt[fmtp]) {
+      case 'd':
+        int d = va_arg(ap, int);
+        offset = num2str(out + outp, d);
+        outp += offset;
+        break;
+      case 's':
+        char* s = va_arg(ap, char*);
+        offset = strcmp(out + outp, s);
+        outp += offset;
+        break;
+      default:
+        panic("not implemented");
+    }
+    fmtp++;
+  }
+  va_end(ap);
+  return outp;
+}
+
+int snprintf(char* out, size_t n, const char* fmt, ...) {
   panic("Not implemented");
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+int vsnprintf(char* out, size_t n, const char* fmt, va_list ap) {
   panic("Not implemented");
 }
 
