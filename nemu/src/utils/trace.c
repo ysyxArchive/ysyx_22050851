46f6599c05434ff210b6f7c663fd4606c574f70d
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-21 18:12:58 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:12:58 up  5:20,  1 user,  load average: 0.00, 0.04, 0.05

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -24,7 +24,7 @@ void print_ring_buf() {
 
 //  mtrace -------------------------------------------------
 void mtrace(bool is_read, paddr_t addr, int len, word_t data) {
-  printf("mtrace: detected memory %s at 0x%08x, the data is \t",
+  Log("detected memory %s at 0x%08x, the data is \t",
          is_read ? "read" : "write", addr);
   for (int i = 7; i >= 0; i--) {
     if (i >= len) {
