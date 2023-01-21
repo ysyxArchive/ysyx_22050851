8e2e7508cdee63262337ef8f5e8f4c2f14060ea1
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-21 17:41:54 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:41:54 up  4:49,  1 user,  load average: 0.31, 0.14, 0.06

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -1,6 +1,7 @@
 #include <common.h>
 #include <string.h>
 
+//  iringbuf -------------------------------------------------
 #define RING_SIZE 16
 
 char iringbuf[RING_SIZE][50] = {0};
@@ -19,4 +20,19 @@ void print_ring_buf() {
     printf("%s\n", iringbuf[p]);
     p = (p + 1) % RING_SIZE;
   }
+}
+
+//  mtrace -------------------------------------------------
+void mtrace(bool is_read, paddr_t addr, int len, word_t data) {
+  printf("mtrace: detected memory %s at 0x%016x, the data is \t",
+         is_read ? "read" : "write", addr);
+  for (int i = 15; i >= 0; i--) {
+    if (i > len) {
+      printf("   ");
+    } else {
+      printf("%02x ", (char)BITS(data, 8 * i + 7, 8 * i));
+    }
+  }
+  printf("\n");
+  return;
 }
