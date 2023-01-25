156c37aa1c69cc619213468d8bbc026092e03aa3
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-26 01:06:42 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:06:42 up 53 min,  1 user,  load average: 0.11, 0.08, 0.09

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -128,5 +128,8 @@ static int decode_exec(Decode *s) {
 
 int isa_exec_once(Decode *s) {
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
-  return decode_exec(s);
+//   return decode_exec(s);
+decode_exec(s);
+printf("%s", s->isa.inst.instname);
+return 0;
 }
