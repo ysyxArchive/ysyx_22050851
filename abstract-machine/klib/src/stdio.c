2728cea205b17af92272cd3adb82af9f90e3a8ca
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-27 18:26:30 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-PA 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:26:30 up  5:07,  1 user,  load average: 0.06, 0.02, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -30,7 +30,37 @@ int num2str(char* out, int num) {
 }
 
 int printf(const char* fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  size_t fmtp = 0;
+  size_t cnt = 0;
+  char bufs[30];
+  while (fmt[fmtp]) {
+    if (fmt[fmtp] != '%') {
+      putch(fmt[fmtp]);
+    } else {
+      fmtp++;
+      switch (fmt[fmtp]) {
+        case 'd':
+          int d = va_arg(ap, int);
+          num2str(bufs, d);
+          for (size_t p = 0; bufs[p]; p++) {
+            putch(bufs[p]);
+          }
+          break;
+        case 's':
+          char* s = va_arg(ap, char*);
+          for (size_t p = 0; s[p]; p++) {
+            putch(s[p]);
+          }
+          break;
+        default:
+      }
+      fmtp++;
+    }
+  }
+  va_end(ap);
+  return cnt;
 }
 
 int vsprintf(char* out, const char* fmt, va_list ap) {
