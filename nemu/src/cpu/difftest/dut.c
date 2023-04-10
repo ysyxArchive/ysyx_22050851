9779e798decbcfef2e037db22a4fe00f1d303773
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-10 17:15:18 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:15:18 up  7:55,  2 users,  load average: 0.22, 0.34, 0.34

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -101,9 +101,9 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
 
 void difftest_step(vaddr_t pc, vaddr_t npc) {
   CPU_state ref_r;
-  Log("difftest step,%lx, %d, %d\n",cpu.pc, skip_dut_nr_inst, is_skip_ref);
   if (skip_dut_nr_inst > 0) {
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
+  Log("difftest step,%lx, %d, %d\n",ref_r.pc, skip_dut_nr_inst, is_skip_ref);
     if (ref_r.pc == npc) {
       skip_dut_nr_inst = 0;
       checkregs(&ref_r, npc);
