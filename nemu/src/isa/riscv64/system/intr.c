d529c51332bbbf1d13e2d15dc53f1dbc3d2abb97
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-10 14:16:30 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:16:30 up  4:56,  2 users,  load average: 0.41, 0.39, 0.31

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -20,8 +20,8 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector.
    */
   csrs("mepc") = cpu.pc;
-  csrs("mstatus") = 1; // TODO: change
-  csrs("mcause") = NO; // TODO: wtf
+  csrs("mstatus") = 0xa00001801;
+  csrs("mcause") = NO;
   return epc;
 }
 
