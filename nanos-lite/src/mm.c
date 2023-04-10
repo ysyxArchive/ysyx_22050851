abb4346a382497d0338cbf79d50687984ca363c3
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-10 10:25:48 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:25:48 up  1:05,  2 users,  load average: 0.43, 0.16, 0.14

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -22,6 +22,7 @@ int mm_brk(uintptr_t brk) {
 }
 
 void init_mm() {
+  Log("free physical pages starting from %p", heap.start);
   pf = (void *)ROUNDUP(heap.start, PGSIZE);
   Log("free physical pages starting from %p", pf);
 
