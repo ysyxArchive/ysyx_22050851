6d3b0c8bad9df1aaaa8e9abb8209cb214ddc401a
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-10 17:28:23 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:28:23 up  8:08,  2 users,  load average: 0.71, 0.91, 0.67

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -22,7 +22,7 @@ bool isa_difftest_checkregs(CPU_state* ref_r, vaddr_t pc) {
   for (int i = 0; i < 32; i++) {
     ans &= difftest_check_reg(reg_name(i, 64), pc, ref_r->gpr[i], cpu.gpr[i]);
   }
-  ans &= difftest_check_reg("pc", pc, ref_r->pc, cpu.pc);
+  ans &= difftest_check_reg("pc", pc, ref_r->pc, pc);
   
   return ans;
 }
