d219d77e50c52d6206cdb487a6ff8e2a209eb5ef
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 02:26:16 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:26:16 up 5 days, 10:42,  2 users,  load average: 0.17, 0.11, 0.16

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,29 +1,21 @@
 #include <memory.h>
 
- void *pf = NULL;
+void *pf = NULL;
 
-void* new_page(size_t nr_page) {
-  return NULL;
-}
+void *new_page(size_t nr_page) { return NULL; }
 
 #ifdef HAS_VME
-static void* pg_alloc(int n) {
-  return NULL;
-}
+static void *pg_alloc(int n) { return NULL; }
 #endif
 
-void free_page(void *p) {
-  panic("not implement yet");
-}
+void free_page(void *p) { panic("not implement yet"); }
 
 /* The brk() system call handler. */
-int mm_brk(uintptr_t brk) {
-  return 0;
-}
+int mm_brk(uintptr_t brk) { return 0; }
 
 void init_mm() {
   pf = (void *)ROUNDUP(heap.start, PGSIZE);
-  Log("free physical pages starting from %p", pf);
+  Log("free physical pages starting from %p", (uint64_t)pf);
 
 #ifdef HAS_VME
   vme_init(pg_alloc, free_page);
