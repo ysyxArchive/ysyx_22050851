ab533bc1375c1f71a88852cd0299b5201df5cc07
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 02:33:22 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:33:22 up 5 days, 10:49,  2 users,  load average: 0.16, 0.24, 0.23

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,7 +11,6 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  printf("%x\n", (uint32_t)(uint64_t)pf);
   Elf_Ehdr elfHeader;
   ramdisk_read(&elfHeader, 0, sizeof(elfHeader));
   Assert(elfHeader.e_ident[0] == ELFMAG0 && elfHeader.e_ident[1] == ELFMAG1 &&
@@ -19,17 +18,22 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
          "error file not elf");
 
   Elf_Phdr prog_header_buf;
+  uint64_t entryAddr = 0;
   for (int i = 0; i < elfHeader.e_phnum; i++) {
     ramdisk_read(&prog_header_buf, elfHeader.e_phoff, sizeof(prog_header_buf));
     if (prog_header_buf.p_type != PT_LOAD) {
       continue;
     }
-    ramdisk_read((uint8_t *)pf + prog_header_buf.p_offset,
+    if (i == 0) {
+      entryAddr = prog_header_buf.p_offset;
+    }
+    ramdisk_read((uint8_t *)pf + prog_header_buf.p_offset - (uint64_t)pf,
                  prog_header_buf.p_offset, prog_header_buf.p_filesz);
-    memset((uint8_t *)pf + prog_header_buf.p_offset + prog_header_buf.p_filesz,
+    memset((uint8_t *)pf + prog_header_buf.p_offset + prog_header_buf.p_filesz -
+               (uint64_t)pf,
            0, prog_header_buf.p_memsz - prog_header_buf.p_filesz);
   }
-  return (uintptr_t)pf;
+  return entryAddr;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
