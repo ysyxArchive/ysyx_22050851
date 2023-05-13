f7d183f4671d0e9c640447d466f6ffb024fa75d3
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-13 22:38:26 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:38:26 up  4:49,  3 users,  load average: 0.21, 0.29, 0.20

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -40,6 +40,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %x", (uint64_t)entry);
+  Log("Jump to entry = %x", (uint32_t)entry);
   ((void (*)())entry)();
+  Log("Jumpout");
+
 }
