4f27dcdc571e86cdea9b4ab55efd36b59e7a7cc7
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-14 05:26:08 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:26:07 up 11:37,  3 users,  load average: 0.21, 0.25, 0.31

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,15 +1,36 @@
 #include <common.h>
+
+enum {
+  SYS_exit,
+  SYS_yield,
+  SYS_open,
+  SYS_read,
+  SYS_write,
+  SYS_kill,
+  SYS_getpid,
+  SYS_close,
+  SYS_lseek,
+  SYS_brk,
+  SYS_fstat,
+  SYS_time,
+  SYS_signal,
+  SYS_execve,
+  SYS_fork,
+  SYS_link,
+  SYS_unlink,
+  SYS_wait,
+  SYS_times,
+  SYS_gettimeofday
+};
+
 static Context *do_event(Event e, Context *c) {
   switch (e.event) {
   case EVENT_YIELD:
     Log("Triggered, id = %d", c->GPR1);
     switch (c->GPR1) {
-    case EVENT_YIELD:
+    case SYS_yield:
       Log("Triggered YIELD");
-      // yield();
-      break;
-    case EVENT_SYSCALL:
-      Log("Triggered SYSTEMCALL");
+      yield();
       break;
     default:
       Panic("Unhandled triggered ID = %d", c->GPR1);
