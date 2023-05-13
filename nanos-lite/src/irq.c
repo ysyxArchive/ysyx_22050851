e7566cf41c864c2195031b40e2c986106b30b0f9
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-14 05:59:37 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:59:37 up 12:11,  2 users,  load average: 0.50, 0.48, 0.70

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -23,19 +23,35 @@ enum {
   SYS_gettimeofday
 };
 
+static size_t sys_write(int fd, char *buf, size_t count) {
+  bool isout = fd == 1 || fd == 2;
+  if (isout) {
+    for (size_t c = 0; c < count; c++) {
+      putch(buf[c]);
+    }
+  }
+  return count;
+}
+
 static Context *do_event(Event e, Context *c) {
   switch (e.event) {
   case EVENT_YIELD:
     Log("Triggered, id = %d", c->GPR1);
     switch (c->GPR1) {
     case SYS_exit:
+      Log("syscall SYS_exit");
       halt(0);
       break;
     case SYS_yield:
+      Log("syscall SYS_yield");
       yield();
       break;
+    case SYS_write:
+      Log("syscall SYS_write");
+      c->GPRx = sys_write(c->GPR2, (char *)c->GPR3, c->GPR4);
+      break;
     case -1:
-      Log("event ID = -1, do nothing");
+      Log("syscall -1, do nothing");
       break;
     default:
       Panic("Unhandled triggered ID = %d", c->GPR1);
