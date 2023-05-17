57e08076401272151df018eeca96df7171713026
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-18 03:47:53 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:47:53 up 1 day,  2:04,  2 users,  load average: 0.53, 0.42, 0.43

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -5,7 +5,7 @@ int main() {
   // write(1, "Hello World!\n", 13);
   int ret = printf("%d\n", 123);
   fflush(stdout);
-  printf("%d", ret);
+  printf("%d", ret < 0);
   int i = 2;
   // volatile int j = 0;
   // while (1) {
