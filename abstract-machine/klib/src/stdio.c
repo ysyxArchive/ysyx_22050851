1e7de28c60b22ba40bd4e91685c9f032004cf7bb
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-30 16:34:46 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:46 up 6 days, 12:54,  2 users,  load average: 0.10, 0.05, 0.11

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -61,7 +61,8 @@ int check_indent(const char *str, uint64_t data, char **ret) {
     case 'x':
       zero_padding = str[0] == '0';
       width = str2num(str, p);
-      num2str(buffer_string, data, zero_padding, width, str[p] == 'd' ? 10 : 16, false);
+      num2str(buffer_string, data, zero_padding, width, str[p] == 'd' ? 10 : 16,
+              false);
       *ret = buffer_string;
       return p + 1;
     case 'p':
@@ -96,12 +97,14 @@ int printf(const char *fmt, ...) {
   while (fmt[fmtp]) {
     if (fmt[fmtp] != '%') {
       putch(fmt[fmtp++]);
+      cnt++;
     } else {
       fmtp++;
       char *rets;
       fmtp += check_indent(fmt + fmtp, va_arg(ap, uint64_t), &rets);
       for (int i = 0; rets[i]; i++) {
         putch(rets[i]);
+        cnt++;
       }
     }
   }
