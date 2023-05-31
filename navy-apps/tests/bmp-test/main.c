59272968bc3bfa759868117eb747df8ba8359c9f
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-31 14:24:10 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:24:10 up 7 days, 10:44,  2 users,  load average: 0.29, 0.70, 0.64

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -1,19 +1,22 @@
-#include <stdio.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <NDL.h>
 #include <BMP.h>
+#include <NDL.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
   NDL_Init(0);
   int w, h;
+  printf("start bmpload\n");
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  printf("end bmpload\n");
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
   NDL_DrawRect(bmp, 0, 0, w, h);
   free(bmp);
   NDL_Quit();
   printf("Test ends! Spinning...\n");
-  while (1);
+  while (1)
+    ;
   return 0;
 }
