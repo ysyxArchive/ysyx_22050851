d227bbcee3735f1f7f6b2cdc99f455b377b97c69
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-01 23:35:43 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:35:43 up 8 days, 19:55,  2 users,  load average: 0.43, 1.12, 1.05

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/bmp-test");
+  naive_uload(NULL, "/bin/nslide");
 }
 
 Context *schedule(Context *prev) { return NULL; }
