0ae0f310ebf4ae5c6a285f6eea87e791a0241aac
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-01 23:26:33 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:26:33 up 8 days, 19:46,  2 users,  load average: 1.71, 1.14, 0.88

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -32,6 +32,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
+  putch('a');
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) { status->ready = true; }
