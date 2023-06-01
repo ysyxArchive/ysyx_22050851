5776146bebdfcec8cbc17301695e75aa3d21ee1e
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-01 22:50:30 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:50:30 up 8 days, 19:10,  2 users,  load average: 0.35, 0.18, 0.06

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -61,19 +61,20 @@ typedef struct {
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int left_offset = (window_w - screen_w) / 2;
   int top_offset = (window_h - screen_h) / 2;
-  //   am_rect rect = {.x = x + left_offset,
-  //                   .y = y + top_offset,
-  //                   .w = w,
-  //                   .h = h,
-  //                   .pixel = pixels};
-  //   fwrite(&rect, sizeof(am_rect), 1, fbdev);
-  for (int row = 0; row < h; row++) {
-    fseek(fbdev,
-          ((top_offset + y + row) * window_w + x + left_offset) *
-              sizeof(uint32_t),
-          SEEK_SET);
-    fwrite(pixels + w * row, sizeof(uint32_t), w, fbdev);
-  }
+  am_rect rect = {.x = x + left_offset,
+                  .y = y + top_offset,
+                  .w = w,
+                  .h = h,
+                  .pixel = pixels};
+  write(fbdev, &rect, sizeof(am_rect));
+  fflush(fbdev);
+  //   for (int row = 0; row < h; row++) {
+  //     fseek(fbdev,
+  //           ((top_offset + y + row) * window_w + x + left_offset) *
+  //               sizeof(uint32_t),
+  //           SEEK_SET);
+  //     fwrite(pixels + w * row, sizeof(uint32_t), w, fbdev);
+  // }
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {}
@@ -123,7 +124,7 @@ int NDL_Init(uint32_t flags) {
     evtdev = 3;
   }
   evtdev = open("/dev/events", "r");
-  fbdev = fopen("/dev/fb", "w");
+  fbdev = open("/dev/fb", "w");
   // read display info
   int dispConfigFile = fopen("/dev/dispinfo", "r");
   char buf[100];
@@ -137,6 +138,6 @@ int NDL_Init(uint32_t flags) {
 }
 
 void NDL_Quit() {
-  fclose(evtdev);
-  fclose(fbdev);
+  close(evtdev);
+  close(fbdev);
 }
