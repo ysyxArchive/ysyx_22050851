79ecb750d952f37131dc4c5f3eb80467680d50fb
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-01 18:53:56 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:53:56 up 8 days, 15:13,  2 users,  load average: 1.47, 2.40, 2.43

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
 #include <NDL.h>
+#include <stdio.h>
 
 int main() {
   NDL_Init(0);
   while (1) {
     char buf[64];
     if (NDL_PollEvent(buf, sizeof(buf))) {
-      printf("receive event: %s\n", buf);
+      if (buf[4] != 'O')
+        printf("receive event: %s\n", buf);
     }
   }
   return 0;
