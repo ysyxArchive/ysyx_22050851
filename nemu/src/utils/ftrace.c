2f62e6bf34d17d10de6b665207834e120c0f0daf
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-04 14:02:15 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:02:15 up 12:33,  2 users,  load average: 0.69, 0.33, 0.14

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -4,7 +4,7 @@
 #include <malloc.h>
 #include <stdio.h>
 #include <string.h>
-#define FRACE_MAX_TRACE 20
+#define FRACE_MAX_TRACE 50
 
 typedef struct FuncNode {
   uint64_t name_index;
@@ -84,18 +84,25 @@ void init_ftrace(char *elflocation[], const int elfCount) {
 }
 
 void prune() {
+  int depth = 0;
+  int maxdepth = -1;
+  PositionNode *p_to_free = NULL;
   if (positionLength > FRACE_MAX_TRACE) {
     PositionNode *p = positionNode.next;
     while (p && p->next && p->next->next && p->next->next->next) {
-      if (!p->next->isret && p->next->next->isret) {
-        PositionNode *q = p->next;
-        p->next = p->next->next->next;
-        free(q->next);
-        free(q);
-        positionLength -= 2;
-        break;
+      if (!p->next->isret && p->next->next->isret && depth > maxdepth) {
+        p_to_free = p;
+        maxdepth = depth;
       }
+      depth += p->isret ? -1 : 1;
       p = p->next;
+    }
+    if (p_to_free) {
+      PositionNode *q = p_to_free->next;
+      p_to_free->next = p_to_free->next->next->next;
+      free(q->next);
+      free(q);
+      positionLength -= 2;
     }
   }
 }
