d9f32cec1d9c086b7ce22365beda28c7e598d6e9
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-20 16:38:53 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux dggua-Standard-PC-i440FX-PIIX-1996 5.19.0-45-generic #46~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 7 15:06:04 UTC 20 x86_64 x86_64 x86_64 GNU/Linux  16:38:53 up  6:16,  2 users,  load average: 0.09, 0.24, 0.18

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -134,27 +134,27 @@ static inline fixedpt fixedpt_divi(fixedpt A, int B) { return A / B; }
 
 /* Multiplies two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
-  printf("mul of %d %d is %ld\n", A  >> FIXEDPT_FBITS,B  >> FIXEDPT_FBITS, (((fixedptd)A * B) >> FIXEDPT_FBITS) >> FIXEDPT_FBITS);
+  printf("mul of %f %f is %f\n", fixedpt_tofloat(A),fixedpt_tofloat(B), fixedpt_tofloat(((fixedptd)A * B) >> FIXEDPT_FBITS));
   return ((fixedptd)A * B) >> FIXEDPT_FBITS;
 }
 
 /* Divides two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_div(fixedpt A, fixedpt B) {
-  printf("div of %d %d is %ld\n", A  >> FIXEDPT_FBITS,B  >> FIXEDPT_FBITS, (((fixedptd)A << FIXEDPT_FBITS) / B) >> FIXEDPT_FBITS);
+  printf("div of %f %f is %f\n", fixedpt_tofloat(A),fixedpt_tofloat(B), fixedpt_tofloat(((fixedptd)A << FIXEDPT_FBITS) / B));
   return ((fixedptd)A << FIXEDPT_FBITS) / B;
 }
 
 static inline fixedpt fixedpt_abs(fixedpt A) { 
-  printf("abs of %d is %d\n", A  >> FIXEDPT_FBITS, (A >= 0 ? A : -A) >> FIXEDPT_FBITS);
+  printf("abs of %f is %f\n", fixedpt_tofloat(A), fixedpt_tofloat(A >= 0 ? A : -A));
   return A >= 0 ? A : -A; }
 
 static inline fixedpt fixedpt_floor(fixedpt A) {
-  printf("floor of %d is %d\n", A  >> FIXEDPT_FBITS, (A >> FIXEDPT_FBITS));
+  printf("floor of %d is %d\n", fixedpt_tofloat(A), (A >> FIXEDPT_FBITS));
   return (A >> FIXEDPT_FBITS) << FIXEDPT_FBITS;
 }
 
 static inline fixedpt fixedpt_ceil(fixedpt A) {
-  printf("ceil of %d is %d\n", A  >> FIXEDPT_FBITS, ((A >> FIXEDPT_FBITS) << FIXEDPT_FBITS) +
+  printf("ceil of %f is %d\n", fixedpt_tofloat(A), ((A >> FIXEDPT_FBITS) << FIXEDPT_FBITS) +
          ((A & FIXEDPT_FMASK) ? FIXEDPT_ONE : 0) >> FIXEDPT_FBITS);
   return ((A >> FIXEDPT_FBITS) << FIXEDPT_FBITS) +
          ((A & FIXEDPT_FMASK) ? FIXEDPT_ONE : 0);
