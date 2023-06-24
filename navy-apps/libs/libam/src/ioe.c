b7ef785f18c038c1f9204dacc5de36fbb8c4d13b
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-24 18:09:59 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:09:59 up  3:45,  1 user,  load average: 0.92, 0.46, 0.34

--- navy-apps/libs/libam/src/ioe.c
+++ navy-apps/libs/libam/src/ioe.c
@@ -1,20 +1,58 @@
 #include <am.h>
-
-bool ioe_init() {
-  return true;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+char buffer[100];
+#define SAMESTR(s, consts) !strncmp(s, consts, strlen(consts))
+void get_dispinfo(AM_GPU_CONFIG_T* configInfo) {
+  int disp = open("/dev/dispinfo", "r");
+  assert(disp);
+  read(disp, buffer, 100);
+  close(disp);
+  char *token = strtok(buffer, "\n");
+  while (token != NULL) {
+    int i = 0;
+    int keyend = 0;
+    for (; token[i]; i++) {
+      if (token[i] == ' ' || token[i] == ':') {
+        keyend = i;
+        break;
+      }
+    }
+    if (keyend) {
+      while (token[i] == ' ' || token[i] == ':') {
+        i++;
+      }
+      int val = 0;
+      int ret = sscanf(token + i, "%d", &val);
+      if(ret == 1){
+        if(SAMESTR(token, "WIDTH")){
+          configInfo->width = val;
+        }
+        if(SAMESTR(token, "HEIGHT")){
+          configInfo->height = val;
+        }
+      }
+    }
+  }
+  return 0;
 }
 
-void ioe_read (int reg, void *buf) {
-  switch (reg)
-  {
+bool ioe_init() { return true; }
+
+void ioe_read(int reg, void *buf) {
+  switch (reg) {
+    case AM_GPU_CONFIG:
+      get_dispinfo(buf);
+      break;
   default:
     printf("tryint to read from %d but not recongized\n", reg);
     break;
   }
- }
-void ioe_write(int reg, void *buf) { 
-  switch (reg)
-  {
+}
+void ioe_write(int reg, void *buf) {
+  switch (reg) {
   default:
     printf("tryint to write to %d but not recongized\n", reg);
     break;
