2c7505a61cda13eda0ef0e11c1847ec3f876ebd8
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-24 14:22:30 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:22:30 up  1:00,  1 user,  load average: 0.28, 0.21, 0.16

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/pal");
+  naive_uload(NULL, "/bin/bird");
 }
 
 Context *schedule(Context *prev) { return NULL; }
