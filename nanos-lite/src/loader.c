73b757594f0643148a9d5d4a0ae040a210c1963e
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-25 15:01:56 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DESKTOP-69F3CE4 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:01:56 up  4:33,  1 user,  load average: 0.10, 0.07, 0.01

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -40,6 +40,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
+  reset_fs();
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", (void *)entry);
   ((void (*)())entry)();
