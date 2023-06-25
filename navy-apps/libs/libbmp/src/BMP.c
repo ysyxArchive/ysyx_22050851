c5ba98d9fa3779c6dabb5372ff652ff05a8ce417
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-25 15:08:15 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DESKTOP-69F3CE4 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:08:15 up  4:39,  1 user,  load average: 0.16, 0.11, 0.03

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -26,7 +26,8 @@ void* BMP_Load(const char *filename, int *width, int *height) {
 
   struct BitmapHeader hdr;
   assert(sizeof(hdr) == 54);
-  assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
+  printf("%d\n", fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
+  // assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
   if (hdr.bitcount != 24) return NULL;
   if (hdr.compression != 0) return NULL;
   int w = hdr.width;
