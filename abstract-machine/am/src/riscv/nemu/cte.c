de08bad35b8c1a7f3246020cd254f80ae004b812
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-28 00:05:28 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DESKTOP-69F3CE4 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:05:28 up  1:36,  1 user,  load average: 0.06, 0.02, 0.00

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,14 +9,14 @@ Context *__am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
     case 11:
-      ev.event = EVENT_YIELD;
+      // ev.event = EVENT_YIELD;
+      c = user_handler(ev, c);
       c->mepc += 4;
       break;
     default:
       ev.event = EVENT_ERROR;
       break;
     }
-    c = user_handler(ev, c);
 
     assert(c != NULL);
   }
