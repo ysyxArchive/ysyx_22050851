2f2534315c3dac89038cb0a3052668400fd3e320
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-28 13:09:58 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DESKTOP-69F3CE4 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:09:58 up  1:37,  1 user,  load average: 0.11, 0.08, 0.08

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -38,7 +38,6 @@ static size_t sys_brk(void *addr) { return 0; }
 #endif
 
 static Context *do_event(Event e, Context *c) {
-  printf("hi, event is %d\n", c->GPR1);
   switch (e.event) {
   case EVENT_YIELD:
     switch (c->GPR1) {
