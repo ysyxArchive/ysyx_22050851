c599f9c728dfa43c89a68fd4280d93ac2aeb411f
ldz <dg_gua@foxmail.com>
2023-06-28 17:11:26 +0800

    context 2

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -5,23 +5,23 @@
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
-
+int pcbcount = 0;
 void switch_boot_pcb() { current = &pcb_boot; }
-int j = 1;
 
 void hello_fun(void *arg) {
+  int j = 1;
   while (1) {
-    Log("Hello World from Nanos-lite with arg '%p' for the %dth time!",
+    Log("Hello World from Nanos-lite with arg '%s' for the %dth time!",
         (uintptr_t)arg, j);
     j++;
     yield();
   }
 }
 
-void context_kload(void *entry, void *arg)
-{
-  Area area = {.start=&pcb[0], .end=&pcb[1]};
-  pcb->cp = kcontext(area, entry, arg);
+void context_kload(void *entry, void *arg) {
+  Area area = {.start=&pcb[pcbcount], .end=&pcb[pcbcount + 1]};
+  pcb[pcbcount].cp = kcontext(area, entry, arg);
+  pcbcount += 1;
 }
 
 
@@ -29,7 +29,8 @@ void init_proc() {
   // switch_boot_pcb();
 
   Log("Initializing processes...");
-  context_kload(hello_fun, NULL);
+  context_kload(hello_fun, "p1");
+  context_kload(hello_fun, "p2");
   switch_boot_pcb();
   // // load program here
   // naive_uload(NULL, "/bin/menu");
@@ -37,6 +38,7 @@ void init_proc() {
 
 Context *schedule(Context *prev) { 
   current->cp  = prev;
-  current = &(pcb[0]);
+  printf("%x %x %x\n", current, pcb, pcb + 1);
+  current = current == &(pcb[0]) ? &(pcb[1]) : &(pcb[0]);
   return current->cp;
 }
