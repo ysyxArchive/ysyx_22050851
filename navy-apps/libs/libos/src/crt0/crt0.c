ff5d8b34dec0214e7e9fed958d53cf514fb74821
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-30 00:11:19 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DESKTOP-69F3CE4 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:11:19 up  6:01,  1 user,  load average: 0.21, 0.19, 0.18

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -7,8 +7,10 @@ int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   printf("%lx, %ld", args, *((uint64_t*)args));
-  char *empty[] =  {NULL };
-  environ = empty;
-  exit(main(0, empty, empty));
+  int argc = *((uint64_t*)args);
+  char **argv =  (char**)*((uint64_t*)args + 1);
+  printf("%s\n", argv[0]);
+  environ = (char**)*((uint64_t*)args + 2 + argc);
+  exit(main(argc, argv, environ));
   assert(0);
 }
