1a287425b06617ef3715a2d9397e5a41326c0ad2
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-30 00:01:06 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DESKTOP-69F3CE4 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:01:06 up  5:51,  1 user,  load average: 0.38, 0.34, 0.20

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -37,7 +37,7 @@ void context_uload(PCB *pcb, const char *filename) {
   *((uint64_t*)(heap.end - strlen(skiparg) - 1) - 4) = (uint64_t)heap.end;
   *((uint64_t*)(heap.end - strlen(skiparg) - 1) - 5) = 1l;
   printf("%x, %d\n", ((uint64_t*)(heap.end - strlen(skiparg) - 1) - 5), *((uint64_t*)(heap.end - strlen(skiparg) - 1) - 5));
-  pcb->cp->GPRx = (uint64_t)((uint64_t*)(heap.end - strlen(skiparg) - 1) - 6);
+  pcb->cp->GPRx = (uint64_t)((uint64_t*)(heap.end - strlen(skiparg) - 1) - 10);
   // pcb->cp->GPRx = (uint64_t)heap.end;
 
 }
