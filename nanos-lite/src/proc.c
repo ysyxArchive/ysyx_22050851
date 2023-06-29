6d07ecddef10feb7c4433010ab84a4ad41a8bc41
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-30 00:13:01 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DESKTOP-69F3CE4 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:13:01 up  6:03,  1 user,  load average: 0.04, 0.13, 0.16

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -35,7 +35,6 @@ void context_uload(PCB *pcb, const char *filename) {
   *((uint64_t*)(heap.end - strlen(skiparg) - 1) - 2) = (uint64_t)NULL;
   *((uint64_t*)(heap.end - strlen(skiparg) - 1) - 3) = (uint64_t)(heap.end - strlen(skiparg) - 1);
   *((uint64_t*)(heap.end - strlen(skiparg) - 1) - 4) = 1;
-  printf("%x, %d\n", ((uint64_t*)(heap.end - strlen(skiparg) - 1) - 4), *((uint64_t*)(heap.end - strlen(skiparg) - 1) - 4));
   pcb->cp->GPRx = (uint64_t)((uint64_t*)(heap.end - strlen(skiparg) - 1) - 4);
   // pcb->cp->GPRx = (uint64_t)heap.end;
 
--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -8,7 +8,7 @@ extern char **environ;
 void call_main(uintptr_t *args) {
   printf("%lx, %ld", args, *((uint64_t*)args));
   int argc = *((uint64_t*)args);
-  char **argv =  (char**)*((uint64_t*)args + 1);
+  char **argv = (char**)*((uint64_t*)args + 1);
   printf("%s\n", argv[0]);
   environ = (char**)*((uint64_t*)args + 2 + argc);
   exit(main(argc, argv, environ));
