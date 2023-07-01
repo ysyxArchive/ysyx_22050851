b3a979b49b2bdab8d6f96eb12fa913351f8f206d
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-01 22:00:04 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DESKTOP-69F3CE4 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:00:04 up 59 min,  1 user,  load average: 0.02, 0.06, 0.03

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -6,7 +6,6 @@ uint8_t* page_end = NULL;
 void *new_page(size_t nr_page) { 
   uint8_t* ret = page_end;
   page_end -= nr_page * PGSIZE;
-  printf("%d %d\n", (int)nr_page, (int)PGSIZE);
   printf("from %x to %x\n", (uint32_t)(uint64_t)ret, (uint32_t)(uint64_t)page_end);
   return ret;
 }
