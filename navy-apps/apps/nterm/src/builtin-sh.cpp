eb81047fa76d9819ccbeb3409e6b9d32a3e55e1a
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-02 16:09:01 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:09:01 up  3:37,  1 user,  load average: 0.82, 0.88, 0.71

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -1,13 +1,11 @@
+#include <SDL.h>
 #include <nterm.h>
 #include <stdarg.h>
 #include <unistd.h>
-#include <SDL.h>
 char buffer[100];
 char handle_key(SDL_Event *ev);
 
-static void sh_init(){
-  setenv("PATH", "/bin/:/usr/bin/", 1);
-}
+static void sh_init() { setenv("PATH", "/bin/:/usr/bin/", 1); }
 static void sh_printf(const char *format, ...) {
   static char buf[256] = {};
   va_list ap;
@@ -21,22 +19,26 @@ static void sh_banner() {
   sh_printf("Built-in Shell in NTerm (NJU Terminal)\n\n");
 }
 
-static void sh_prompt() {
-  sh_printf("sh> ");
-}
+static void sh_prompt() { sh_printf("sh> "); }
 
 static void sh_handle_cmd(const char *cmd) {
   int len = strlen(cmd);
   assert(len < 100);
   strncpy(buffer, cmd, len - 1);
-  char* program = strtok(buffer, " ");
-  char* argv[100];
+  char *program = strtok(buffer, " ");
+  char *argv[100];
   int argc = 0;
-  char* p;
-  while((p = strtok(NULL, " "))){
-    printf("builtin: %s\n");
-    argv[argc++] = p;
-    assert(argc < 100);
+  char *p = strtok(buffer, " ");
+  int i = 0;
+  while (p != NULL) {
+    if (i == 0) {
+    } else {
+      argv[argc++] = p;
+      assert(argc < 100);
+      printf("builtin: %s\n", p);
+    }
+    p = strtok(NULL, " ");
+    i++;
   }
   argv[argc] = NULL;
 
