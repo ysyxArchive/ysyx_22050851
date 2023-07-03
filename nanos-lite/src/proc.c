80b94225bb84bd664b23a977eac23e94972c3017
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-03 22:52:53 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:52:53 up  5:04,  1 user,  load average: 0.34, 0.27, 0.33

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,6 +27,8 @@ void context_kload(PCB *pcb, void *entry, void *arg) {
 
 
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]) {
+  Assert(argv, "argv is NULL when executing %s", filename);
+  Assert(envp, "envp is NULL when executing %s", filename);
   void* stack = new_page(8);
   Area area = {.start=pcb, .end=pcb + 1};
   uintptr_t entry = loader(pcb, filename);
