ccd3ecd06d05962ef7e2736951e43afdfe0be12d
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-04 11:48:55 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DESKTOP-69F3CE4 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:48:55 up 3 min,  1 user,  load average: 0.49, 0.49, 0.22

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -9,7 +9,7 @@ void call_main(uintptr_t *args) {
   int argc = *((uint64_t*)args);
   char **argv = ((uint64_t*)args + 1);
   environ = (char**)((uint64_t*)args + argc + 1);
-  printf("environ: %s", *environ);
+  printf("environnnnnn: %s", *environ);
   exit(main(argc, argv, environ));
   assert(0);
 }
