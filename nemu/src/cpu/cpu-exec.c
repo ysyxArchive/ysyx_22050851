593618c0cbfe52ed863b4dec667206fd2e6b7d5e
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-11 19:22:10 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:22:10 up 23:01,  1 user,  load average: 0.21, 0.48, 0.57

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -62,16 +62,14 @@ static void exec_once(Decode *s, vaddr_t pc) {
   // update pc
   cpu.pc = s->dnpc;
 
-  // #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
-  IFDEF(CONFIG_ITRACE,
-        p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc));
+  p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
   int ilen = s->snpc - s->pc;
   int i;
-  IFDEF(CONFIG_ITRACE, uint8_t *inst = (uint8_t *)&s->isa.inst.val);
+  uint8_t *inst = (uint8_t *)&s->isa.inst.val;
 
   for (i = ilen - 1; i >= 0; i--) {
-    IFDEF(CONFIG_ITRACE, p += snprintf(p, 4, " %02x", inst[i]));
+    p += snprintf(p, 4, " %02x", inst[i]);
   }
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -13,8 +13,8 @@
 # See the Mulan PSL v2 for more details.
 #**************************************************************************************/
 
-ifdef CONFIG_ITRACE
+# ifdef CONFIG_ITRACE
 CXXSRC = src/utils/disasm.cc
 CXXFLAGS += $(shell llvm-config --cxxflags) -fPIE
 LIBS += $(shell llvm-config --libs)
-endif
+# endif
