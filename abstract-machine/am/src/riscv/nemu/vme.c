fd113af84f8e03d1c046c95fb4b13696fe911fcd
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-11 17:33:02 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:33:02 up 22:06,  1 user,  load average: 0.36, 1.17, 1.46

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -50,7 +50,7 @@ bool vme_init(void *(*pgalloc_f)(int), void (*pgfree_f)(void *)) {
 }
 
 void protect(AddrSpace *as) {
-  PTE *updir = (PTE *)(pgalloc_usr(PGSIZE));
+  PTE *updir = (PTE *)(pgalloc_usr(1));
   as->ptr = updir;
   as->area = USER_SPACE;
   as->pgsize = PGSIZE;
