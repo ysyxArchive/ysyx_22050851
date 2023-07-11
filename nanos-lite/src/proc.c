03651e472cb0a85b40ee3e2b034dfcfa897a4797
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-11 19:45:48 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:45:48 up 23:24,  1 user,  load average: 0.39, 0.24, 0.35

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -1,7 +1,7 @@
+#include "fs.h"
 #include <loader.h>
 #include <proc.h>
 #include <string.h>
-#include "fs.h"
 #define MAX_NR_PROC 4
 
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
@@ -9,95 +9,96 @@ static PCB pcb_boot = {};
 PCB *current = NULL;
 int pcbcount = 0;
 void switch_boot_pcb() { current = &pcb_boot; }
-PCB* executing[2];
+PCB *executing[2];
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    if(j % 200 == 0)
-    Log("Hello World from Nanos-lite with arg '%s' for the %dth time!",
-        (uintptr_t)arg, j);
+    if (j % 200 == 0)
+      Log("Hello World from Nanos-lite with arg '%s' for the %dth time!",
+          (uintptr_t)arg, j);
     j++;
     yield();
   }
 }
 
 void context_kload(PCB *pcb, void *entry, void *arg) {
-  Area area = {.start=pcb, .end=pcb + 1};
+  Area area = {.start = pcb, .end = pcb + 1};
   pcb->cp = kcontext(area, entry, arg);
 }
 
-
-void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]) {
+void context_uload(PCB *pcb, const char *filename, char *const argv[],
+                   char *const envp[]) {
   Log("loading bin file %s", filename);
   Assert(argv, "argv is NULL when executing %s", filename);
   Assert(envp, "envp is NULL when executing %s", filename);
   reset_fs();
-  Area area = {.start=pcb, .end=pcb + 1};
+  Area area = {.start = pcb, .end = pcb + 1};
   uintptr_t entry = loader(pcb, filename);
   pcb->cp = ucontext(&(pcb->as), area, (void *)entry);
   uint64_t offsetCount = 0;
   int argc = 0;
   int envc = 0;
-  void* stack = pcb->stack + STACK_SIZE;
-  for(int i = 0; envp[i]; i++) {
+  void *stack = pcb->stack + STACK_SIZE;
+  for (int i = 0; envp[i]; i++) {
     envc += 1;
-    offsetCount += strlen(envp[i]) + 1;  
+    offsetCount += strlen(envp[i]) + 1;
     strcpy(stack - offsetCount, envp[i]);
   }
-  for(int i = 0; argv[i]; i++) {
+  for (int i = 0; argv[i]; i++) {
     argc += 1;
     offsetCount += strlen(argv[i]) + 1;
     strcpy(stack - offsetCount, argv[i]);
   }
-  
+
   int tempOffset = 0;
-  *((uint64_t*)(stack - offsetCount) - 1) = (uint64_t)NULL;
+  while (((uint64_t)stack - offsetCount) % 4) {
+    offsetCount++;
+  }
+  *((uint64_t *)(stack - offsetCount) - 1) = (uint64_t)NULL;
   offsetCount += sizeof(uint64_t);
-  for(int i = 0; envp[i]; i++) {
+  for (int i = 0; envp[i]; i++) {
     tempOffset += strlen(envp[i]) + 1;
-    *((uint64_t*)(stack - offsetCount) - 1) = (uint64_t)(stack - tempOffset);
+    *((uint64_t *)(stack - offsetCount) - 1) = (uint64_t)(stack - tempOffset);
     offsetCount += sizeof(uint64_t);
-  }  
-  *((uint64_t*)(stack - offsetCount) - 1) = (uint64_t)NULL;
+  }
+  *((uint64_t *)(stack - offsetCount) - 1) = (uint64_t)NULL;
   offsetCount += sizeof(uint64_t);
-  for(int i = 0; argv[i]; i++) {
+  for (int i = 0; argv[i]; i++) {
     tempOffset += strlen(argv[i]) + 1;
-    uint64_t* reversedp = (uint64_t*)(stack - offsetCount) - argc + i + i; 
+    uint64_t *reversedp = (uint64_t *)(stack - offsetCount) - argc + i + i;
     *reversedp = (uint64_t)(stack - tempOffset);
     offsetCount += sizeof(uint64_t);
   }
-  *((uint64_t*)(stack - offsetCount) - 1) = argc;
+  *((uint64_t *)(stack - offsetCount) - 1) = argc;
   offsetCount += sizeof(uint64_t);
   pcb->cp->GPRx = (uint64_t)(stack - offsetCount);
 }
 
-PCB* getPCB() {
-  return &(pcb[pcbcount++]);
-}
+PCB *getPCB() { return &(pcb[pcbcount++]); }
 
 void init_proc() {
   Log("Initializing processes...");
-  char target_program[] = "/bin/dummy";  
+  char target_program[] = "/bin/dummy";
   executing[0] = getPCB();
   context_kload(executing[0], hello_fun, "p2");
-  char* args[] = {target_program, NULL};
-  char* envp[] = {NULL};
+  char *args[] = {target_program, NULL};
+  char *envp[] = {NULL};
   executing[1] = getPCB();
   context_uload(executing[1], target_program, args, envp);
   switch_boot_pcb();
 }
 
-void replacePCB(PCB* newone){
-  for (int i = 0; i < 2; i++){
-    if(executing[i] == current){
+void replacePCB(PCB *newone) {
+  for (int i = 0; i < 2; i++) {
+    if (executing[i] == current) {
       executing[i] = newone;
       return;
     }
   }
 }
 
-Context *schedule(Context *prev) { 
-  current->cp  = prev;
+Context *schedule(Context *prev) {
+  current->cp = prev;
   current = current == executing[0] ? executing[1] : executing[0];
   return current->cp;
 }
