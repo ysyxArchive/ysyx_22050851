3d3ff0e83740d1f8047d3df01dd16be1ffa3ba10
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-12 15:31:51 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:31:51 up 16 min,  1 user,  load average: 0.51, 0.34, 0.46

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -97,6 +97,7 @@ void sim_t::diff_memcpy(reg_t dest, void *src, size_t n) {
 extern "C" {
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
+  Log("%x", addr);
   if (direction == DIFFTEST_TO_REF) {
     s->diff_memcpy(addr, buf, n);
   } else {
