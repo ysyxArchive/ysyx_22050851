58271ddebc7128bf22c4fb6ac2c6491f2047aec6
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-12 20:13:07 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:13:07 up  4:26,  1 user,  load average: 0.60, 0.67, 0.52

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -83,13 +83,13 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   // 二级页表
   if (!PTEVALID(((PTE *)as->ptr)[vpn[2]])) {
     uintptr_t newpage = (uintptr_t)pgalloc_usr(1);
-    ((PTE *)as->ptr)[vpn[2]] = (BITS(newpage, 55, 12) << 10) | 1 | 0x11;
+    ((PTE *)as->ptr)[vpn[2]] = (BITS(newpage, 55, 12) << 10) | 1;
   }
   PTE *table1 = (PTE *)PTEPPN(((PTE *)(as->ptr))[vpn[2]]);
   // 三级页表
   if (!PTEVALID(table1[vpn[1]])) {
     uintptr_t newpage = (uintptr_t)pgalloc_usr(1);
-    table1[vpn[1]] = (BITS(newpage, 55, 12) << 10) | 1 | 0x11;
+    table1[vpn[1]] = (BITS(newpage, 55, 12) << 10) | 1;
   }
   PTE *table2 = (PTE *)PTEPPN(table1[vpn[1]]);
   table2[vpn[0]] = BITS((uintptr_t)pa, 55, 12) << 10 | 0x1F; //1 | (0x7 << 1) | ((!!prot) << 4);
