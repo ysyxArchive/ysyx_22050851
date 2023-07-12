a04635e2681d2d7305736b2ef49f5c748e1bfedc
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-12 17:22:07 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:22:07 up  2:07,  1 user,  load average: 0.26, 0.14, 0.10

--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -37,7 +37,7 @@ static void restart() {
 void init_isa() {
   /* Load built-in image. */
   memcpy(guest_to_host(RESET_VECTOR), img, sizeof(img));
-  csrs("mstatus") = 0xa00001800;
+  csrs("mstatus") = 0xa000c1800;
   /* Initialize this virtual computer system. */
   restart();
 }
