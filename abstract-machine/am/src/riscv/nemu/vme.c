522f3bb3c552decebfa7fff0f288dc34220efc64
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-13 19:21:11 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:21:11 up 12:10,  1 user,  load average: 1.37, 0.85, 0.41

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -98,7 +98,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
 
   Context c = {
-      .mepc = (uint64_t)entry, .mstatus = 0xa000c0000, .pdir = as->ptr};
+      .mepc = (uint64_t)entry, .mstatus = 0xa000c0008, .pdir = as->ptr};
   memcpy(kstack.start, &c, sizeof(c));
   return kstack.start;
 }
