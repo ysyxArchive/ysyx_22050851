4bdfe4615959cb2d998efeb0c5a7ac7252040517
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-13 15:48:44 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:48:44 up  9:35,  1 user,  load average: 0.13, 0.24, 0.18

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -27,7 +27,7 @@ int mm_brk(uintptr_t brk) {
   if (brk > current->max_brk) {
     unsigned addr_to_alloc = brk - current->max_brk;
     unsigned pages_to_alloc =
-        addr_to_alloc / PGSIZE + addr_to_alloc % PGSIZE != 0;
+        addr_to_alloc / PGSIZE + (addr_to_alloc % PGSIZE != 0);
     uint64_t addr = (uintptr_t)pg_alloc(pages_to_alloc);
     for (int i = 0; i < pages_to_alloc; i++) {
       map(&(current->as), (void *)(current->max_brk + i * PGSIZE),
