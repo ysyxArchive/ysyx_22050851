e73a840a5336e8bd4f134043f70554e8a28503f6
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-13 16:02:03 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:02:03 up  9:48,  1 user,  load average: 0.21, 0.16, 0.16

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -99,6 +99,6 @@ void replacePCB(PCB *newone) {
 
 Context *schedule(Context *prev) {
   current->cp = prev;
-  current = current == executing[0] ? executing[1] : executing[1];
+  current = current == executing[0] ? executing[1] : executing[0];
   return current->cp;
 }
