aac85575458ee571cd16a0ee77b1053b1a61002e
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-13 19:51:51 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux DGGua-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:51:51 up 12:41,  1 user,  load average: 4.84, 4.33, 2.32

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,7 +7,7 @@ extern void __am_switch(Context *c);
 
 Context *__am_irq_handle(Context *c) {
   __am_get_cur_as(c);
-    printf("%x\n", c->mcause);
+  printf("%x\n", c->mcause);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
