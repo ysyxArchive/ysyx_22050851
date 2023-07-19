9fc424b47c65fee3593b23d541219989ba0321b5
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-19 21:23:51 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:51 up 1 day,  9:38,  0 users,  load average: 0.70, 0.55, 0.53

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -35,16 +35,16 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 #define IRQ_TIMER 0x8000000000000007
 
 word_t isa_query_intr() {
-  // if (!(csrs("mstatus") & 0x8)) {
-  //   return INTR_EMPTY;
-  // }
-  // clock_t end = clock();
-  // double elapsed = ((double)(end - start)) / CLOCKS_PER_SEC; // 计算经过的秒数
-  // if (elapsed >= 1) { // 每秒一次的时钟中断
-  //   Log("trigger!");
-  //   start = end;
-  //   cpu.INTR = false;
-  //   return IRQ_TIMER;
-  // }
+  if (!(csrs("mstatus") & 0x8)) {
+    return INTR_EMPTY;
+  }
+  clock_t end = clock();
+  double elapsed = ((double)(end - start)) / CLOCKS_PER_SEC; // 计算经过的秒数
+  if (elapsed >= 1) { // 每秒一次的时钟中断
+    Log("trigger!");
+    start = end;
+    cpu.INTR = false;
+    return IRQ_TIMER;
+  }
   return INTR_EMPTY;
 }
