336d5a8267845d38cbd15e413eef334b04e00f3d
ldz <dg_gua@foxmail.com>
2023-07-22 19:55:38 +0800

    complete PA4

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,5 +1,4 @@
 #include <common.h>
-
 #if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
 #define MULTIPROGRAM_YIELD() yield()
 #else
@@ -8,35 +7,42 @@
 
 #define NAME(key) [AM_KEY_##key] = #key,
 static AM_GPU_CONFIG_T gpuConfig;
-
-static const char *keyname[256]
+extern int fppcb;
+static const char* keyname[256]
     __attribute__((used)) = {[AM_KEY_NONE] = "NONE", AM_KEYS(NAME)};
 
-size_t serial_write(const void *buf, size_t offset, size_t len) {
+size_t serial_write(const void* buf, size_t offset, size_t len) {
   yield();
-  ((char *)buf)[len] = 0;
+  ((char*)buf)[len] = 0;
   for (int i = 0; i < len; i++) {
-    putch(((char *)buf)[i]);
+    putch(((char*)buf)[i]);
   }
   return len;
 }
 
-size_t events_read(void *buf, size_t offset, size_t len) {
+size_t events_read(void* buf, size_t offset, size_t len) {
   yield();
   AM_INPUT_KEYBRD_T keyboardEvent;
   ioe_read(AM_INPUT_KEYBRD, &keyboardEvent);
+  if (keyboardEvent.keydown &&
+      strcmp(keyname[keyboardEvent.keycode], "F1") == 0) {
+    fppcb = 2;
+  } else if (keyboardEvent.keydown &&
+             strcmp(keyname[keyboardEvent.keycode], "F2") == 0) {
+    fppcb = 3;
+  }
   return sprintf(buf, "%s %s %d\n", keyboardEvent.keydown ? "kd" : "ku",
                  keyname[keyboardEvent.keycode], keyboardEvent.keycode);
 }
 
-size_t dispinfo_read(void *buf, size_t offset, size_t len) {
+size_t dispinfo_read(void* buf, size_t offset, size_t len) {
   yield();
   ioe_read(AM_GPU_CONFIG, &gpuConfig);
   return sprintf(buf, "WIDTH : %d\nHEIGHT:    %d\n", gpuConfig.width,
                  gpuConfig.height);
 }
 
-size_t fb_write(AM_GPU_FBDRAW_T *buf, size_t offset, size_t len) {
+size_t fb_write(AM_GPU_FBDRAW_T* buf, size_t offset, size_t len) {
   yield();
   ioe_write(AM_GPU_FBDRAW, buf);
   return len;
