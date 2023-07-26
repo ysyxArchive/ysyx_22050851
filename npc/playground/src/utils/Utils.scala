24df47fba68b1274a74e5ef484ccb2a98a7ca9d6
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-26 22:45:16 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:45:16 up 3 days,  9:02,  0 users,  load average: 0.65, 0.70, 0.70

--- npc/playground/src/utils/Utils.scala
+++ npc/playground/src/utils/Utils.scala
@@ -4,8 +4,6 @@ import chisel3._
 import chisel3.experimental.ChiselEnum
 import chisel3.util._
 import firrtl.seqCat
-import decode.Source
-import decode.SourceType
 import execute.ALUUtils
 
 object Utils {
@@ -21,7 +19,4 @@ object Utils {
   def signExtend(num: UInt, width: Int, targetWidth: Int = 64): UInt = {
     Cat(Fill(targetWidth - width, num(width - 1)), num(width - 1, 0))
   }
-  def isRegType(source: Source): Bool =
-    source.stype === SourceType.reg.asUInt || source.stype === SourceType.regLow.asUInt
-
 }
