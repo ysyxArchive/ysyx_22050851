a4d3a3b63badfae61d89de7a08444257e3ebd758
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-26 15:46:55 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:46:55 up 3 days,  2:04,  0 users,  load average: 0.48, 0.64, 0.57

--- npc/playground/src/InstructionExecuteUnit.scala
+++ npc/playground/src/InstructionExecuteUnit.scala
@@ -12,7 +12,8 @@ class InstructionExecuteUnit extends Module {
   val memIO = IO(Flipped(new MemIO()))
   val regIO = IO(Flipped(new RegisterFileIO()))
 
-  val decodeIn = RegNext(in.bits, DecodeOut.default())
+  // val decodeIn = RegNext(in.bits, DecodeOut.default())
+  val decodeIn = in.bits
   val controlIn = decodeIn.control
   val dataIn    = decodeIn.data
 
