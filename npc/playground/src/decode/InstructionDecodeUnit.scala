bc0bebd8a78f5ea7c75677e5f60b9af268981a43
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-27 00:03:11 +0800

    > sim RTL ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:03:11 up 3 days, 10:20,  0 users,  load average: 2.92, 1.59, 0.87

--- npc/playground/src/decode/InstructionDecodeUnit.scala
+++ npc/playground/src/decode/InstructionDecodeUnit.scala
@@ -23,6 +23,7 @@ class DecodeOut extends Bundle {
 }
 
 object DecodeOut {
+  val default = new D
   def default() = {
     val defaultout = Wire(new DecodeOut)
     defaultout.control := DecodeControlOut.default()
