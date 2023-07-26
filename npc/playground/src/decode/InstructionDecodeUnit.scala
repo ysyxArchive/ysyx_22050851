9ab688b8cff9ae7c37078025a9cf5ee726cd7e20
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-27 00:03:30 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:03:30 up 3 days, 10:20,  0 users,  load average: 2.52, 1.58, 0.88

--- npc/playground/src/decode/InstructionDecodeUnit.scala
+++ npc/playground/src/decode/InstructionDecodeUnit.scala
@@ -23,7 +23,7 @@ class DecodeOut extends Bundle {
 }
 
 object DecodeOut {
-  val default = new DecodeOut().Lit()
+  val default = new DecodeOut().Lit(_.
   def default() = {
     val defaultout = Wire(new DecodeOut)
     defaultout.control := DecodeControlOut.default()
