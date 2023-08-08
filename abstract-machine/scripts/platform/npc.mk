ac3213e609ab72007ef44406b84240bb93b5b099
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-08 19:37:44 +0800

    > sim RTL ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:37:44 up 7 days,  9:34,  0 users,  load average: 1.91, 2.69, 1.66

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -26,7 +26,7 @@ image: $(IMAGE).elf
 
 run: image
 
-	$(MAKE) -C $(NPC_HOME) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
+	echo $(mainargs) && $(MAKE) -C $(NPC_HOME) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
 
 batch: image
 	$(MAKE) -C $(NPC_HOME) ISA=$(ISA) batch ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
