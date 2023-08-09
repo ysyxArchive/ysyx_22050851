231d40dbf405a8ea0080c3255ac5ad9586b5f012
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-09 16:39:53 +0800

    > sim RTL ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:39:53 up 8 days,  6:37,  0 users,  load average: 0.49, 0.69, 0.70

--- npc/csrc/utils.cpp
+++ npc/csrc/utils.cpp
@@ -78,7 +78,9 @@ void init_vcd_trace() {
   top = new VCPU{contextp};
   top->reset = false;
   top->trace(tfp, 0);
-  tfp->open("wave.vcd");       // 打开vcd
+  tfp->rolloverSize(1e4);
+  tfp->open("wave.vcd"); // 打开vcd
+
   top->pcio_inst = 0x00000013; // 默认为 addi e0, 0;
 }
 
@@ -88,10 +90,10 @@ void eval_trace() {
   top->eval();
   tfp->dump((npc_clock++) % 10000);
   tfp->flush();
-  if (npc_clock % 10000 == 0) {
-    printf("%d\n", npc_clock);
-    tfp->open("wave.vcd"); // 打开vcd
-  }
+  // if (npc_clock % 10000 == 0) {
+  //   printf("%d\n", npc_clock);
+  //   tfp->openNext("wave.vcd"); // 打开vcd
+  // }
   if (npc_clock > 122128) {
     assert(0);
   }
