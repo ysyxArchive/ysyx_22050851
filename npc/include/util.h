334d16cd7e001fed3071efc4e7c2a58751be5d1c
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-09 16:36:34 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:36:34 up 8 days,  6:33,  0 users,  load average: 0.78, 0.56, 0.65

--- npc/include/util.h
+++ npc/include/util.h
@@ -36,24 +36,25 @@
 
 #define ANSI_FMT(str, fmt) fmt str ANSI_NONE
 
-#define log_write(...)                  \
-  IFDEF(                                \
-      CONFIG_TARGET_NATIVE_ELF, do {    \
-        extern FILE* log_fp;            \
-        extern bool log_enable();       \
-        if (log_enable()) {             \
-          fprintf(log_fp, __VA_ARGS__); \
-          fflush(log_fp);               \
-        }                               \
+#define log_write(...)                                                         \
+  IFDEF(                                                                       \
+      CONFIG_TARGET_NATIVE_ELF, do {                                           \
+        extern FILE *log_fp;                                                   \
+        extern bool log_enable();                                              \
+        if (log_enable()) {                                                    \
+          fprintf(log_fp, __VA_ARGS__);                                        \
+          fflush(log_fp);                                                      \
+        }                                                                      \
       } while (0))
 
-#define _Log(...)           \
-  do {                      \
-    printf(__VA_ARGS__);    \
+#define _Log(...)                                                              \
+  do {                                                                         \
+    printf(__VA_ARGS__);                                                       \
   } while (0)
-    // log_write(__VA_ARGS__); \
+// log_write(__VA_ARGS__); \
 
-int parse_args(int argc, char* argv[]);
+int parse_args(int argc, char *argv[]);
 void load_files();
-
+void init_vcd_trace();
+void eval_trace();
 #endif
