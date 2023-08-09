58e846dd147cf58682b31d4ab09eb864f9022ad2
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-09 14:08:22 +0800

    > sim RTL ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:08:22 up 8 days,  4:05,  0 users,  load average: 0.66, 0.50, 0.72

--- npc/csrc/device.cpp
+++ npc/csrc/device.cpp
@@ -1,27 +1,6 @@
-#include <SDL2/SDL.h>
 #include <common.h>
-#include <device.h>
 #include <sys/time.h>
 static uint64_t start = 0;
-SDL_Window* window;
-uint32_t vga_data[VGA_HEIGHT * VGA_WIDTH];
-
-void init_device() {
-  SDL_Init(SDL_INIT_VIDEO);
-  window = SDL_CreateWindow("riscv-npc", SDL_WINDOWPOS_CENTERED,
-                            SDL_WINDOWPOS_CENTERED, VGA_WIDTH, VGA_HEIGHT,
-                            SDL_WINDOW_SHOWN);
-}
-
-void update_vga() {
-  SDL_Surface* surface = SDL_GetWindowSurface(window);
-
-  SDL_Surface* screen =
-      SDL_CreateRGBSurfaceFrom(vga_data, VGA_WIDTH, VGA_HEIGHT, 32, 4,
-                               0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF);
-  SDL_BlitSurface(screen, NULL, surface, NULL);
-}
-
 uint64_t gettime() {
   struct timeval now;
   if (start == 0) {
