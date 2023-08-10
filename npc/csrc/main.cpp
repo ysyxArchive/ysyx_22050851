a24e122c34ff8d8898079f54d6c47b0f9fbb8b7d
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-10 20:12:22 +0800

    > sim RTL ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:12:22 up 9 days, 10:09,  0 users,  load average: 0.56, 0.53, 0.50

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -83,10 +83,6 @@ void one_step() {
   if ((npc_clock / 2) % LIGHT_SSS_CYCLE_INTERVAL == 0) {
     lightSSS.do_fork();
   }
-  if (npc_clock > 12322333) {
-    is_halt = true;
-    is_bad_halt = true;
-  }
 }
 
 int main(int argc, char *argv[]) {
