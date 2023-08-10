f585484e82d8e77459a96d8b09f738c3a90537b6
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-10 20:11:37 +0800

    > sim RTL ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:11:37 up 9 days, 10:08,  0 users,  load average: 0.51, 0.52, 0.49

--- npc/csrc/device/device.cpp
+++ npc/csrc/device/device.cpp
@@ -53,6 +53,7 @@ void update_device() {
   while (SDL_PollEvent(&event)) {
     switch (event.type) {
     case SDL_QUIT:
+      Log("SDL_QUIT");
       is_halt = true;
       is_bad_halt = true;
       break;
