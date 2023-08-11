a4b31156be535d12011bb275dc46642614c38bea
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-12 01:07:47 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:07:47 up 10 days, 15:05,  0 users,  load average: 2.28, 1.17, 0.87

--- npc/playground/src/ControlRegisterFile.scala
+++ npc/playground/src/ControlRegisterFile.scala
@@ -2,17 +2,22 @@ import chisel3._
 import chisel3.util.{is, switch, MuxLookup}
 import chisel3.util.Decoupled
 import decode.DecodeControlOut
+import decode.CsrSource
+import decode.AluMux1
+import firrtl.seqCat
+import decode.CsrSetMode
+
+class ControlRegisterInfo(val name: String, val id: Int, val initVal: Int = 0)
 
 object ControlRegisterList {
-  def ControlRegister(name: String, id: Int, initVal: Int = 0) = id -> RegInit(initVal.U(64.W))
 
   val list = List(
-    ControlRegister("satp", 0x180),
-    ControlRegister("mstatus", 0x300),
-    ControlRegister("mtvec", 0x305),
-    ControlRegister("mscratch", 0x340),
-    ControlRegister("mepc", 0x341),
-    ControlRegister("mcause", 0x342)
+    new ControlRegisterInfo("satp", 0x180),
+    new ControlRegisterInfo("mstatus", 0x300),
+    new ControlRegisterInfo("mtvec", 0x305),
+    new ControlRegisterInfo("mscratch", 0x340),
+    new ControlRegisterInfo("mepc", 0x341),
+    new ControlRegisterInfo("mcause", 0x342)
   )
 }
 
@@ -27,19 +32,27 @@ class ControlRegisterFileIO extends Bundle {
 class ControlRegisterFile extends Module {
   val io = IO(new ControlRegisterFileIO())
 
-  val registers = ControlRegisterList.list((a, b) => (a, RegInit(b.U(64.W))))
-
-  val writeBack = Wire(64.W)
-
-  for (i <- 0 to ControlRegister.list.len) {
-    ControlRegister.list(i) := Mux(io.csrIndex === i.U, writeBack, ControlRegister.list(i))
-  }
-
-
-  io.output := MuxLookup(
-    csrIndex,
-    DontCare,
-    ControlRegisterList.list
+  val registers = ControlRegisterList.list.map(info => RegInit(info.initVal.U(64.W)))
+  val indexMap  = ControlRegisterList.list.zipWithIndex.map { case (info, index) => info.id.U -> registers(index) }.toMap
+  val mask = MuxLookup(
+    io.control.csrsource,
+    io.src1,
+    Seq(
+      CsrSource.src1.asUInt -> io.src1,
+      CsrSource.uimm.asUInt -> io.uimm
+    )
   )
+
+  indexMap(io.csrIndex) := MuxLookup(
+    io.control.csrsetmode,
+    indexMap(io.csrIndex),
+    Seq(
+      CsrSetMode.clear.asUInt -> (indexMap(io.csrIndex) & ~mask),
+      CsrSetMode.set.asUInt -> (indexMap(io.csrIndex) | mask),
+      CsrSetMode.write.asUInt -> mask
+    )
+  )
+
+  io.output := indexMap(io.csrIndex)
 
 }
