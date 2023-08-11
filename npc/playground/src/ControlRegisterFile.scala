c113c757d08032f07c23ee568533215d47fb6961
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-12 00:39:02 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:39:02 up 10 days, 14:36,  0 users,  load average: 0.56, 0.90, 0.83

--- npc/playground/src/ControlRegisterFile.scala
+++ npc/playground/src/ControlRegisterFile.scala
@@ -1,16 +1,18 @@
 import chisel3._
 import chisel3.util.{is, switch, MuxLookup}
-
-class ControlRegisterInfo(val name: String, val id: Int, val initVal: Int = 0) {}
+import chisel3.util.Decoupled
+import decode.DecodeControlOut
 
 object ControlRegisterList {
+  def ControlRegister(name: String, id: Int, initVal: Int = 0) = id -> RegInit(initVal.U(64.W))
+
   val list = List(
-    new ControlRegisterInfo("satp", 0x180),
-    new ControlRegisterInfo("mstatus", 0x300),
-    new ControlRegisterInfo("mtvec", 0x305),
-    new ControlRegisterInfo("mscratch", 0x340),
-    new ControlRegisterInfo("mepc", 0x341),
-    new ControlRegisterInfo("mcause", 0x342)
+    ControlRegister("satp", 0x180),
+    ControlRegister("mstatus", 0x300),
+    ControlRegister("mtvec", 0x305),
+    ControlRegister("mscratch", 0x340),
+    ControlRegister("mepc", 0x341),
+    ControlRegister("mcause", 0x342)
   )
 }
 
@@ -18,12 +20,26 @@ class ControlRegisterFileIO extends Bundle {
   val src1     = Input(UInt(64.W))
   val csrIndex = Input(UInt(12.W))
   val uimm     = Input(UInt(5.W))
+  val control  = Input(new DecodeControlOut())
   val output   = Output(UInt(64.W))
 }
 
 class ControlRegisterFile extends Module {
   val io = IO(new ControlRegisterFileIO())
 
-  val registers = ControlRegisterList.list.map(info -> RegInit(info.initVal.U(64.W)))
+  val registers = ControlRegisterList.list((a, b) => (a, RegInit(b.U(64.W))))
+
+  val writeBack = Wire(64.W)
+
+  for (i <- 0 to ControlRegister.list.len) {
+    ControlRegister.list(i) := Mux(io.csrIndex === i.U, writeBack, ControlRegister.list(i))
+  }
+
+
+  io.output := MuxLookup(
+    csrIndex,
+    DontCare,
+    ControlRegisterList.list
+  )
 
 }
