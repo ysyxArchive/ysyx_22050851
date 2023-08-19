6cafab5cb06bba9e32f612b2dc7a386a027ae57c
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-19 14:41:34 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:41:34 up 18 days,  4:38,  0 users,  load average: 0.65, 0.73, 0.56

--- npc/playground/src/execute/InstructionExecuteUnit.scala
+++ npc/playground/src/execute/InstructionExecuteUnit.scala
@@ -137,7 +137,8 @@ class InstructionExecuteUnit extends Module {
   memOut := Mux(
     controlIn.memmode === MemMode.read.asUInt,
     Utils.signExtend(memIO.rdata, memlen << 3),
-    Utils.zeroExtend(memIO.rdata, memlen << 3)
+    Utils.signExtend(memIO.rdata, memlen << 3)
+    // Utils.zeroExtend(memIO.rdata, memlen << 3)
   )
   memIO.wdata := src2
   // blackBoxHalt
