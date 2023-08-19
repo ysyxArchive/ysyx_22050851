67ef87e2e38d1d93ccad14a3a2aaa467b05f15fc
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-19 14:38:05 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:38:05 up 18 days,  4:35,  0 users,  load average: 0.73, 0.63, 0.49

--- npc/playground/src/utils/Utils.scala
+++ npc/playground/src/utils/Utils.scala
@@ -21,35 +21,39 @@ object Utils {
     Cat(Fill(targetWidth - num.getWidth, 0.U), num(width - 1, 0))
   }
   def zeroExtend(num: UInt, width: UInt): UInt = {
+    val ret = Wire(UInt(64.W))
     when(width === 8.U) {
-      return Utils.zeroExtend(num, 8, 64)
+      ret := Utils.zeroExtend(num, 8, 64)
     }.elsewhen(width === 16.U) {
-      return Utils.zeroExtend(num, 16, 64)
+      ret := Utils.zeroExtend(num, 16, 64)
     }.elsewhen(width === 32.U) {
-      return Utils.zeroExtend(num, 32, 64)
+      ret := Utils.zeroExtend(num, 32, 64)
     }.elsewhen(width === 64.U) {
-      return Utils.zeroExtend(num, 64, 64)
+      ret := Utils.zeroExtend(num, 64, 64)
     }.otherwise {
+      ret := 0.U
       throw new Error("width not implemented")
     }
-    return 0.U
+    ret
   }
   def signExtend(num: UInt, width: Int, targetWidth: Int = 64): UInt = {
     Cat(Fill(targetWidth - width, num(width - 1)), num(width - 1, 0))
   }
   def signExtend(num: UInt, width: UInt): UInt = {
+    val ret = Wire(UInt(64.W))
     when(width === 8.U) {
-      return Utils.signExtend(num, 8, 64)
+      ret := Utils.signExtend(num, 8, 64)
     }.elsewhen(width === 16.U) {
-      return Utils.signExtend(num, 16, 64)
+      ret := Utils.signExtend(num, 16, 64)
     }.elsewhen(width === 32.U) {
-      return Utils.signExtend(num, 32, 64)
+      ret := Utils.signExtend(num, 32, 64)
     }.elsewhen(width === 64.U) {
-      return Utils.signExtend(num, 64, 64)
+      ret := Utils.signExtend(num, 64, 64)
     }.otherwise {
+      ret := 0.U
       throw new Error("width not implemented")
     }
-    return 0.U
+    ret
   }
 }
 
