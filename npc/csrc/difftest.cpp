64d93dd78d79a7f300e0c837e53168352f831fbd
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-19 10:45:13 +0800

    > sim RTL ysyx_22050851 李东哲 Linux daa3082d5276 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:45:13 up 18 days, 42 min,  0 users,  load average: 0.87, 0.71, 0.54

--- npc/csrc/difftest.cpp
+++ npc/csrc/difftest.cpp
@@ -64,7 +64,7 @@ void difftest_step(CPU *cpu) {
     difftest_exec(1);
   }
 }
-// when pc changed, last pc pointered inst must  
+// when pc changed, last pc pointered inst must be executed done 
 static CPU lastcpu;
 static CPU refcpu;
 bool difftest_check(CPU *cpu) {
