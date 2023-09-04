157dc6216def21d152fb1ad8ad073c6b1099816d
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-05 00:05:55 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:05:55 up 6 days,  3:15,  0 users,  load average: 3.99, 1.94, 1.10

--- npc/playground/src/InstructionFetchUnit.scala
+++ npc/playground/src/InstructionFetchUnit.scala
@@ -1,4 +1,4 @@
-import Chisel.{switch, Cat, Decoupled, DecoupledIO, Fill, Mux1H, MuxCase, MuxLookup}
+import Chisel._
 import chisel3.DontCare.:=
 import chisel3._
 import chisel3.experimental.{BaseModule, ChiselEnum}
