6298c5398355e7b8038ac56c8e34c3ccd7af422a
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-06 20:59:43 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:59:43 up 8 days, 9 min,  0 users,  load average: 2.06, 1.61, 1.01

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -81,4 +81,14 @@ class Cache(totalByte: Int, groupSize: Int, addrWidth: Int = 64) extends Module 
       cacheMem(i)(0).valid := true.B
     }
   }
+
+  axiIO.AW.valid     := DontCare
+  axiIO.AW.bits.id   := DontCare
+  axiIO.AW.bits.addr := DontCare
+  axiIO.AW.bits.prot := DontCare
+  axiIO.W.valid      := DontCare
+  axiIO.W.bits.data  := DontCare
+  axiIO.W.bits.strb  := DontCare
+  axiIO.B.ready      := DontCare
+  axiIO.R.ready      := DontCare
 }
