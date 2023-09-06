3a7f213176ded4b9825f099e713a2b41c6ca598f
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-06 19:26:10 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:26:10 up 7 days, 22:35,  0 users,  load average: 0.38, 0.31, 0.38

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -31,8 +31,8 @@ class Cache(totalByte: Int, groupSize: Int, addrWidth: Int = 64) extends Module 
 
   val io    = IO(new CacheIO())
   val axiIO = IO(new AxiLiteIO(UInt(64.W), 64))
-  val cacheMem = Vec(waycnt, Vec(groupSize, new CacheLine(addrWidth - tagOffset, cellByte)))
-  // val cacheMem = Seq.tabulate(waycnt)(_ => Seq.tabulate(groupSize)(_ => new CacheLine(addrWidth - tagOffset, cellByte)))
+  // val cacheMem = Vec(waycnt, Vec(groupSize, new CacheLine(addrWidth - tagOffset, cellByte)))
+  val cacheMem = Vec(waycnt, Vec(groupSize, Reg(UInt(64.W))))
 
   val hit = Wire(Bool())
 
