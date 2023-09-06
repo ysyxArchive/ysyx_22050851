08e2088e2fc47e2aa0b20af40c716abe8da02912
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-06 15:17:27 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:17:27 up 7 days, 18:26,  0 users,  load average: 1.26, 0.70, 0.81

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -4,6 +4,7 @@ object Elaborate extends App {
   def top = new CPU()
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
   print(args)
+  print("123")
   args :+ "-o=./build"
   (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
 }
