2358e2c25bffd63de00752b30f51bf6b9c9d8990
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-06 21:49:43 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:49:43 up 8 days, 59 min,  0 users,  load average: 2.61, 0.98, 0.64

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -45,9 +45,9 @@ class Cache(totalByte: Int, groupSize: Int, addrWidth: Int = 64) extends Module 
     List(
       (idle, true.B, sendRes),
       (sendRes, io.data.fire, idle),
-      (idle, io.readReq.fire && !hit, sendReq),
-      (sendReq, false.B, waitRes),
-      (waitRes, false.B, sendRes)
+      // (idle, io.readReq.fire && !hit, sendReq),
+      // (sendReq, false.B, waitRes),
+      // (waitRes, false.B, sendRes)
     )
   )
   val d      = cacheFSM.trigger(idle, sendRes)
