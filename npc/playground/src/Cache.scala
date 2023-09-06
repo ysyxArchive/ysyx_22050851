ac2bd0c2e3a72592dc33d87cdd10c4bed57038a8
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-06 19:26:34 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:26:34 up 7 days, 22:35,  0 users,  load average: 0.61, 0.38, 0.40

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -34,47 +34,47 @@ class Cache(totalByte: Int, groupSize: Int, addrWidth: Int = 64) extends Module 
   // val cacheMem = Vec(waycnt, Vec(groupSize, new CacheLine(addrWidth - tagOffset, cellByte)))
   val cacheMem = Vec(waycnt, Vec(groupSize, Reg(UInt(64.W))))
 
-  val hit = Wire(Bool())
+  // val hit = Wire(Bool())
 
-  val idle :: sendRes :: sendReq :: waitRes :: others = Enum(5)
+  // val idle :: sendRes :: sendReq :: waitRes :: others = Enum(5)
 
-  val cacheFSM = new FSM(
-    idle,
-    List(
-      (idle, io.data.fire && hit, sendRes),
-      (sendRes, io.data.fire, idle),
-      (idle, io.data.fire && !hit, sendReq),
-      (sendReq, false.B, waitRes),
-      (waitRes, false.B, sendRes)
-    )
-  )
+  // val cacheFSM = new FSM(
+  //   idle,
+  //   List(
+  //     (idle, io.data.fire && hit, sendRes),
+  //     (sendRes, io.data.fire, idle),
+  //     (idle, io.data.fire && !hit, sendReq),
+  //     (sendReq, false.B, waitRes),
+  //     (waitRes, false.B, sendRes)
+  //   )
+  // )
 
-  val tag    = io.readReq.bits(addrWidth - 1, tagOffset)
-  val index  = io.readReq.bits(tagOffset - 1, indexOffset)
-  val offset = io.readReq.bits(indexOffset - 1, 0)
+  // val tag    = io.readReq.bits(addrWidth - 1, tagOffset)
+  // val index  = io.readReq.bits(tagOffset - 1, indexOffset)
+  // val offset = io.readReq.bits(indexOffset - 1, 0)
 
-  val wayValid = cacheMem(index).map(line => line.valid && line.tag === tag)
+  // val wayValid = cacheMem(index).map(line => line.valid && line.tag === tag)
 
-  hit := wayValid.reduce(_ & _)
+  // hit := wayValid.reduce(_ & _)
 
-  // when idle
-  val addr = Reg(UInt(addrWidth.W))
-  addr          := Mux(cacheFSM.is(idle), io.data.bits, addr)
-  io.data.ready := cacheFSM.is(idle) && io.data.valid
-  // when sendRes
-  val data = Mux1H(wayValid, cacheMem(index)).data
-  val s    = Seq.tabulate(cellByte)(o => ((o.U === offset) -> data(o * 8 - 1, 0)))
-  io.data.bits  := PriorityMux(s)
-  io.data.valid := true.B
-  // when sendReq
-  axiIO.AR.bits  := io.readReq.bits
-  axiIO.AR.valid := Mux(cacheFSM.is(sendReq), io.data.bits, addr)
-  // when waitRes
-  for (i <- 0 to waycnt) {
-    when(cacheFSM.is(waitRes) && index === i.U && axiIO.R.fire) {
-      cacheMem(i)(0).data  := axiIO.R.bits
-      cacheMem(i)(0).tag   := tag
-      cacheMem(i)(0).valid := true.B
-    }
-  }
+  // // when idle
+  // val addr = Reg(UInt(addrWidth.W))
+  // addr          := Mux(cacheFSM.is(idle), io.data.bits, addr)
+  // io.data.ready := cacheFSM.is(idle) && io.data.valid
+  // // when sendRes
+  // val data = Mux1H(wayValid, cacheMem(index)).data
+  // val s    = Seq.tabulate(cellByte)(o => ((o.U === offset) -> data(o * 8 - 1, 0)))
+  // io.data.bits  := PriorityMux(s)
+  // io.data.valid := true.B
+  // // when sendReq
+  // axiIO.AR.bits  := io.readReq.bits
+  // axiIO.AR.valid := Mux(cacheFSM.is(sendReq), io.data.bits, addr)
+  // // when waitRes
+  // for (i <- 0 to waycnt) {
+  //   when(cacheFSM.is(waitRes) && index === i.U && axiIO.R.fire) {
+  //     cacheMem(i)(0).data  := axiIO.R.bits
+  //     cacheMem(i)(0).tag   := tag
+  //     cacheMem(i)(0).valid := true.B
+  //   }
+  // }
 }
