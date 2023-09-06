df72de479f5d86b869e83794a966c794d959d75f
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-06 22:47:59 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:47:59 up 8 days,  1:57,  0 users,  load average: 1.04, 0.81, 0.73

--- npc/playground/src/execute/InstructionExecuteUnit.scala
+++ npc/playground/src/execute/InstructionExecuteUnit.scala
@@ -177,5 +177,5 @@ class InstructionExecuteUnit extends Module {
   blackBox.io.halt     := controlIn.goodtrap
   blackBox.io.bad_halt := controlIn.badtrap || res._2 === false.B
 
-  decodeIn.done := exeStatus === exeIdle
+  decodeIn.done := exeFSM.is(exeIdle) && !exeFSM.willChangeTo(exeWaitMem)
 }
