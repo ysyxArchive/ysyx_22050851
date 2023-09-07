5b786732dba542303af596025dac79630dbf9b52
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-07 17:51:10 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:51:10 up 8 days, 21:00,  0 users,  load average: 0.35, 0.44, 0.39

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -87,8 +87,8 @@ class Cache(cellByte: Int = 64, wayCnt: Int = 2, groupSize: Int = 1, addrWidth: 
   axiIO.AR.bits.prot := 0.U
   axiIO.AR.valid     := cacheFSM.is(sendReq)
   // when waitRes
-  val mask       = Cat(Seq.tabulate(updateTimes)(index => UIntToOH(counter)(index)))
-  val maskedData = Fill(updateTimes, axiIO.R.bits.data.asUInt) & mask
+  val mask       = Cat(Seq.tabulate(updateTimes)(index =>Fill(axiIO.getWidth, UIntToOH(counter)(index)))
+  val maskedData = Fill(updateTimes, axiIO.getWidth.U) & mask
   for (i <- 0 until waycnt) {
     when(cacheFSM.is(waitRes) && index === i.U && axiIO.R.fire) {
       cacheMem(i)(0).data := maskedData & (cacheMem(i)(0).data & ~mask)
