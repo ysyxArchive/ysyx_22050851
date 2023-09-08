f526bd597b00b900fe58b03da66339a3e04a686f
ldz <dg_gua@foxmail.com>
2023-09-08 16:29:36 +0800

    多路、单组多个只读Cache，采用（伪）随机算法替换

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -2,6 +2,7 @@ import chisel3._
 import chisel3.util._
 import utils.FSM
 import decode.AluMode
+import os.group
 
 class CacheIO extends Bundle {
   val readReq = Flipped(Decoupled(UInt(64.W)))
@@ -20,13 +21,13 @@ class CacheLine(tagWidth: Int, dataByte: Int) extends Bundle {
   * @param groupSize 单路单元数
   * @param addrWidth 地址宽度
   */
-class Cache(cellByte: Int = 64, wayCnt: Int = 2, groupSize: Int = 1, addrWidth: Int = 64) extends Module {
-  val totalByte = cellByte * groupSize * wayCnt
-  val cellCnt   = totalByte / cellByte
-  assert(cellCnt % groupSize == 0)
-  val waycnt      = cellCnt / groupSize
-  val indexOffset = log2Up(cellByte)
-  val tagOffset   = log2Up(cellByte) + log2Up(groupSize)
+class Cache(cellByte: Int = 64, wayCnt: Int = 4, groupSize: Int = 4, addrWidth: Int = 64) extends Module {
+  assert(1 << log2Ceil(cellByte) == cellByte)
+  assert(1 << log2Ceil(wayCnt) == wayCnt)
+  assert(1 << log2Ceil(groupSize) == groupSize)
+  val totalByte   = cellByte * groupSize * wayCnt
+  val indexOffset = log2Ceil(cellByte)
+  val tagOffset   = log2Ceil(cellByte) + log2Ceil(wayCnt)
 
   val io    = IO(new CacheIO())
   val axiIO = IO(new AxiLiteIO(UInt(64.W), 64))
@@ -36,7 +37,7 @@ class Cache(cellByte: Int = 64, wayCnt: Int = 2, groupSize: Int = 1, addrWidth: 
 
   val cacheMem = RegInit(
     VecInit(
-      Seq.fill(waycnt)(VecInit(Seq.fill(groupSize)(0.U.asTypeOf(new CacheLine(addrWidth - tagOffset, cellByte)))))
+      Seq.fill(wayCnt)(VecInit(Seq.fill(groupSize)(0.U.asTypeOf(new CacheLine(addrWidth - tagOffset, cellByte)))))
     )
   )
 
@@ -44,7 +45,7 @@ class Cache(cellByte: Int = 64, wayCnt: Int = 2, groupSize: Int = 1, addrWidth: 
 
   val idle :: sendRes :: sendReq :: waitRes :: others = Enum(5)
 
-  val counter = RegInit(0.U(log2Up(updateTimes).W))
+  val counter = RegInit(0.U(log2Ceil(updateTimes).W))
   counter := PriorityMux(
     Seq(
       (counter === updateTimes.U) -> 0.U,
@@ -64,6 +65,9 @@ class Cache(cellByte: Int = 64, wayCnt: Int = 2, groupSize: Int = 1, addrWidth: 
       (waitRes, axiIO.R.fire && (counter === (updateTimes - 1).U), sendRes)
     )
   )
+
+  val replaceIndex = RegInit(0.U(log2Ceil(groupSize).W))
+
   val tag    = io.readReq.bits(addrWidth - 1, tagOffset)
   val index  = io.readReq.bits(tagOffset - 1, indexOffset)
   val offset = io.readReq.bits(indexOffset - 1, 0)
@@ -71,29 +75,33 @@ class Cache(cellByte: Int = 64, wayCnt: Int = 2, groupSize: Int = 1, addrWidth: 
   val wayValid = cacheMem(index).map(line => line.valid && line.tag === tag)
 
   hit := wayValid.reduce(_ & _)
-
+  
   // when idle
   val addr = Reg(UInt(addrWidth.W))
   addr             := Mux(cacheFSM.is(idle), io.readReq.bits, addr)
   io.readReq.ready := cacheFSM.is(idle) && io.readReq.valid
+  replaceIndex     := Mux(cacheFSM.is(idle), Mux(replaceIndex === (groupSize - 1).U, 0.U, replaceIndex + 1.U), replaceIndex)
   // when sendRes
-  val data = Mux1H(wayValid, cacheMem(index)).data
-  val s    = Seq.tabulate(cellByte - 1)(o => ((o.U === offset) -> data(data.getWidth - 1, o * 8)))
+  val line = Mux1H(wayValid, cacheMem(index))
+  val data = line.data
+  val s    = Seq.tabulate(cellByte)(o => ((o.U === offset) -> data(data.getWidth - 1, o * 8)))
   io.data.bits  := PriorityMux(s)
   io.data.valid := cacheFSM.is(sendRes)
   // when sendReq
-  axiIO.AR.bits.addr := Cat(Seq(tag, index, counter << log2Up(axiIO.dataWidth / 8)))
+  axiIO.AR.bits.addr := Cat(Seq(tag, index, counter << log2Ceil(axiIO.dataWidth / 8)))
   axiIO.AR.bits.id   := 0.U
   axiIO.AR.bits.prot := 0.U
   axiIO.AR.valid     := cacheFSM.is(sendReq)
   // when waitRes
   val mask       = Reverse(Cat(Seq.tabulate(updateTimes)(index => Fill(axiIO.dataWidth, UIntToOH(counter)(index)))))
   val maskedData = Fill(updateTimes, axiIO.R.bits.data.asUInt) & mask
-  for (i <- 0 until waycnt) {
+  for (i <- 0 until wayCnt) {
     when(cacheFSM.is(waitRes) && index === i.U && axiIO.R.fire) {
-      cacheMem(i)(0).data  := maskedData | (cacheMem(i)(0).data & ~mask)
-      cacheMem(i)(0).tag   := tag
-      cacheMem(i)(0).valid := true.B
+      cacheMem(i)(replaceIndex).data := maskedData | (cacheMem(i)(replaceIndex).data & ~mask)
+      when(counter === (updateTimes - 1).U) {
+        cacheMem(i)(replaceIndex).tag   := tag
+        cacheMem(i)(replaceIndex).valid := true.B
+      }
     }
   }
   axiIO.R.ready := cacheFSM.is(waitRes)
