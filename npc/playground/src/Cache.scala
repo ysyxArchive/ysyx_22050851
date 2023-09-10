152937c3fd35d75bb71babcf7d0135a0a5073e00
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-10 17:07:54 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:07:54 up 11 days, 20:17,  0 users,  load average: 2.71, 2.44, 2.31

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -176,10 +176,10 @@ class Cache(cellByte: Int = 64, wayCnt: Int = 4, groupSize: Int = 4, addrWidth: 
     val addr = io.addr
     when(io.writeReq.fire) {
       val data = io.writeReq.bits.data
-      printf(cf"cache writing, addr is %x, data is $data\n", addr)
+      printf(cf"cache writing, addr is %d, data is $data\n", addr)
     }
     when(io.readReq.fire) {
-      printf(cf"cache reading, addr is %x\n", addr)
+      printf(cf"cache reading, addr is %d\n", addr)
     }
   }
 }
