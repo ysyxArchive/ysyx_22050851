c16446be1367b8aa0c87916f3e1c515778abf93f
tracer-ysyx2204 <tracer@ysyx.org>
2023-09-11 15:00:57 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:00:57 up 12 days, 18:10,  0 users,  load average: 1.52, 1.19, 1.01

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -149,13 +149,7 @@ class Cache(
   )
   // ...1111110011111...
   val writeMask = ~writePositionMask | (writePositionMask &
-    Reverse(
-      Cat(
-        Seq.tabulate(slotsPerLine)(_ =>
-          Cat(Seq.tabulate(dataWriteReq.mask.getWidth)(index => dataWriteReq.mask(index)))
-        )
-      )
-    ))
+    Reverse(Cat(Seq.tabulate(dataWidth / 8)(index => Fill(8, dataWriteReq.mask(index))))) << (offset * 8.U))
   val maskedWriteData = (dataWriteReq.data << (offset * 8.U)) & ~writeMask
   for (i <- 0 until wayCnt) {
     when(cacheFSM.is(writeData) && index === i.U && cacheMem(i)(targetIndex).valid) {
