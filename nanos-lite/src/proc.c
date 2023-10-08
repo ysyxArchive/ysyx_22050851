9c9a808a4afd59c674d58204a8f4f4859dafeecc
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-08 18:44:21 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:44:21 up 7 days, 47 min,  0 users,  load average: 0.71, 1.11, 1.29

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -108,7 +108,7 @@ void init_proc() {
 
 Context* schedule(Context* prev) {
   // Assert(current, "current is NULL");
-  // current->cp = prev;
+  current->cp = prev;
   // int currentidx = current == &(pcb[fppcb]) ? fppcb : 1;
   // int nextidx = currentidx == 1 ? fppcb : 1;
   // int nextidx = 1;
