7c0b863596928ccbe6daf029afec28a0c97f87e2
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-08 17:44:36 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:44:36 up 6 days, 23:47,  0 users,  load average: 2.02, 1.47, 1.08

--- nemu/include/cpu/difftest.h
+++ nemu/include/cpu/difftest.h
@@ -22,26 +22,30 @@
 #ifdef CONFIG_DIFFTEST
 void difftest_skip_ref();
 void difftest_skip_dut(int nr_ref, int nr_dut);
-void difftest_set_patch(void (*fn)(void *arg), void *arg);
+void difftest_set_patch(void (*fn)(void* arg), void* arg);
 void difftest_step(vaddr_t pc, vaddr_t npc);
 void difftest_detach();
 void difftest_attach();
 #else
 static inline void difftest_skip_ref() {}
 static inline void difftest_skip_dut(int nr_ref, int nr_dut) {}
-static inline void difftest_set_patch(void (*fn)(void *arg), void *arg) {}
+static inline void difftest_set_patch(void (*fn)(void* arg), void* arg) {}
 static inline void difftest_step(vaddr_t pc, vaddr_t npc) {}
-void difftest_detach();
-void difftest_attach();
+void difftest_detach() {}
+void difftest_attach() {}
 #endif
 
-extern void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n,
+extern void (*ref_difftest_memcpy)(paddr_t addr,
+                                   void* buf,
+                                   size_t n,
                                    bool direction);
-extern void (*ref_difftest_regcpy)(void *dut, bool direction);
+extern void (*ref_difftest_regcpy)(void* dut, bool direction);
 extern void (*ref_difftest_exec)(uint64_t n);
 extern void (*ref_difftest_raise_intr)(uint64_t NO);
 
-static inline bool difftest_check_reg(const char *name, vaddr_t pc, word_t ref,
+static inline bool difftest_check_reg(const char* name,
+                                      vaddr_t pc,
+                                      word_t ref,
                                       word_t dut) {
   // TODO: 问题有点大，在yield中如何规避？
   if (ref != dut && (ref ^ dut) != 0x20000) {
