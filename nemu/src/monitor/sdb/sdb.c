e94cf6cd31ef09f96c9e4efcde9dd38b9a5787a7
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-08 17:47:14 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:47:14 up 6 days, 23:50,  0 users,  load average: 0.90, 1.16, 1.01

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -21,9 +21,13 @@
 #include "memory/paddr.h"
 #include "cpu/difftest.h"
 #include "difftest-def.h"
-#include "cpu/difftest.h"
+
+
 extern long img_size;
 void init_difftest(char *ref_so_file, long img_size, int port);
+void difftest_detach();
+void difftest_attach();
+
 
 static bool is_batch_mode = false;
 void init_regex();
