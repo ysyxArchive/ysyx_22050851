31fcace7b9558452b3ab9699f93ef2c28a7c27bf
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-19 13:26:08 +0800

    > generate verilog ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-155-generic #172-Ubuntu SMP Fri Jul 7 16:10:02 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:26:08 up 83 days, 22:06,  3 users,  load average: 11.98, 11.38, 10.94

--- npc/playground/src/execute/InstructionExecuteUnit.scala
+++ npc/playground/src/execute/InstructionExecuteUnit.scala
@@ -23,7 +23,6 @@ class ExeIn extends Bundle {
 class InstructionExecuteUnit extends Module {
   val exeIn      = IO(Flipped(Decoupled(new ExeIn())))
   val exeOut     = IO(Decoupled(new MemRWIn()))
-  val memIO      = IO(Flipped(new CacheIO(64, 64)))
   val regIO      = IO(Flipped(new RegisterFileIO()))
   val csrIn      = IO(Input(UInt(64.W)))
   val csrControl = IO(Flipped(new CSRFileControl()))
@@ -32,25 +31,18 @@ class InstructionExecuteUnit extends Module {
 
   val alu = Module(new ALU)
 
-  val memOut = Wire(UInt(64.W))
-
-  val shouldMemWork = exeIn.bits.control.memmode =/= MemMode.no.asUInt
-
-  val memIsRead     = exeInReg.control.memmode === MemMode.read.asUInt || exeInReg.control.memmode === MemMode.readu.asUInt
   val shouldWaitALU = !alu.io.out.bits.isImmidiate
 
-  val idle :: waitMemReq :: waitMemRes :: waitPC :: waitALU :: other = Enum(10)
+  val idle :: waitDecode :: waitALU :: waitSend :: other = Enum(10)
 
   val exeFSM = new FSM(
     idle,
     List(
-      (idle, exeIn.fire && shouldMemWork, waitMemReq),
-      (idle, exeIn.fire && shouldWaitALU, waitALU),
-      (idle, exeIn.fire, waitPC),
-      (waitALU, alu.io.out.fire, waitPC),
-      (waitMemReq, Mux(memIsRead, memIO.readReq.fire, memIO.writeReq.fire), waitMemRes),
-      (waitMemRes, Mux(memIsRead, memIO.data.fire, memIO.writeRes.fire), waitPC),
-      (waitPC, true.B, idle)
+      (waitDecode, exeIn.fire && shouldWaitALU, waitALU),
+      (waitDecode, exeIn.fire, waitSend),
+      (waitALU, alu.io.out.fire, waitSend),
+      (waitSend, exeOut.fire, idle),
+      (idle, exeOut.ready, waitDecode)
     )
   )
 
@@ -60,7 +52,6 @@ class InstructionExecuteUnit extends Module {
   val src2 = Wire(UInt(64.W))
   regIO.raddr0 := exeInReg.data.src1
   regIO.raddr1 := exeInReg.data.src2
-  regIO.waddr  := Mux(exeInReg.control.regwrite && exeFSM.willChangeTo(waitPC), exeInReg.data.dst, 0.U)
   val snpc = regIO.pc + 4.U
   val pcBranch = MuxLookup(exeInReg.control.pcaddrsrc, false.B)(
     EnumSeq(
@@ -74,8 +65,8 @@ class InstructionExecuteUnit extends Module {
       PCAddrSrc.one -> true.B
     )
   )
-  val csrInReg = RegInit(csrIn)
-  csrInReg := Mux(exeFSM.willChangeTo(waitPC), csrIn, csrInReg)
+  // val csrInReg = RegInit(csrIn)
+  // csrInReg := Mux(exeFSM.willChangeTo(waitPC), csrIn, csrInReg)
   val dnpcAddSrcReg = RegNext(
     MuxLookup(exeInReg.control.pcsrc, regIO.pc)(
       EnumSeq(
@@ -84,25 +75,6 @@ class InstructionExecuteUnit extends Module {
       )
     )
   )
-  val dnpcAlter = MuxLookup(exeInReg.control.pccsr, dnpcAddSrcReg)(
-    EnumSeq(
-      PcCsr.origin -> (dnpcAddSrcReg + exeInReg.data.imm),
-      PcCsr.csr -> csrInReg
-    )
-  )
-  regIO.dnpc := Mux(exeFSM.is(waitPC), Mux(pcBranch.asBool, dnpcAlter, snpc), regIO.pc)
-  val regwdata = MuxLookup(exeInReg.control.regwritemux, alu.io.out.bits.out)(
-    EnumSeq(
-      RegWriteMux.alu -> alu.io.out.bits.out,
-      RegWriteMux.snpc -> snpc,
-      RegWriteMux.mem -> memOut,
-      RegWriteMux.aluneg -> Utils.zeroExtend(alu.io.out.bits.signals.isNegative, 1, 64),
-      RegWriteMux.alunotcarryandnotzero -> Utils
-        .zeroExtend(!alu.io.out.bits.signals.isCarry && !alu.io.out.bits.signals.isZero, 1, 64),
-      RegWriteMux.csr -> csrIn
-    )
-  )
-  regIO.wdata := Mux(exeInReg.control.regwsext, Utils.signExtend(regwdata.asUInt, 32), regwdata)
 
   src1 :=
     Mux(
@@ -136,49 +108,9 @@ class InstructionExecuteUnit extends Module {
   alu.io.out.ready      := alu.io.out.bits.isImmidiate || exeFSM.is(waitALU)
   alu.io.in.valid       := exeIn.fire
   // csr
-  csrControl.csrBehave  := Mux(exeFSM.willChangeTo(waitPC), exeInReg.control.csrbehave, CsrBehave.no.asUInt)
-  csrControl.csrSetmode := Mux(exeFSM.willChangeTo(waitPC), exeInReg.control.csrsetmode, CsrSetMode.origin.asUInt)
-  csrControl.csrSource  := exeInReg.control.csrsource
-
-  // mem
-  val memlen = MuxLookup(exeInReg.control.memlen, 1.U)(
-    EnumSeq(
-      MemLen.one -> 1.U,
-      MemLen.two -> 2.U,
-      MemLen.four -> 4.U,
-      MemLen.eight -> 8.U
-    )
-  )
-
-  val memMask = Cat(
-    Fill(4, Mux(memlen > 4.U, 1.U, 0.U)),
-    Fill(2, Mux(memlen > 2.U, 1.U, 0.U)),
-    Fill(1, Mux(memlen > 1.U, 1.U, 0.U)),
-    1.U(1.W)
-  )
-  val memAddrReg = Reg(UInt(64.W))
-  memAddrReg := Mux(exeFSM.willChangeTo(waitMemReq), alu.io.out.bits.out, memAddrReg)
-
-  memIO.readReq.valid      := exeFSM.is(waitMemReq) && memIsRead && shouldMemWork
-  memIO.addr               := memAddrReg
-  memIO.data.ready         := exeFSM.is(waitMemRes) && memIsRead
-  memIO.writeReq.valid     := exeFSM.is(waitMemReq) && !memIsRead && shouldMemWork
-  memIO.writeReq.bits.data := src2
-  memIO.writeReq.bits.mask := memMask
-  memIO.writeRes.ready     := exeFSM.is(waitMemRes)
-  val memOutRaw = MuxLookup(exeInReg.control.memlen, memIO.data.bits)(
-    EnumSeq(
-      MemLen.one -> memIO.data.asUInt(7, 0),
-      MemLen.two -> memIO.data.asUInt(15, 0),
-      MemLen.four -> memIO.data.asUInt(31, 0),
-      MemLen.eight -> memIO.data.asUInt
-    )
-  )
-  memOut := Mux(
-    exeInReg.control.memmode === MemMode.read.asUInt,
-    Utils.signExtend(memOutRaw, memlen << 3),
-    Utils.zeroExtend(memOutRaw, memlen << 3)
-  )
+  // csrControl.csrBehave  := Mux(exeFSM.willChangeTo(waitPC), exeInReg.control.csrbehave, CsrBehave.no.asUInt)
+  // csrControl.csrSetmode := Mux(exeFSM.willChangeTo(waitPC), exeInReg.control.csrsetmode, CsrSetMode.origin.asUInt)
+  // csrControl.csrSource  := exeInReg.control.csrsource
 
   // blackBoxHalt
   val blackBox = Module(new BlackBoxHalt);
