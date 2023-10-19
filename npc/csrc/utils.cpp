90bb92324ff69a39142123d60449463aad519a5f
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-19 19:31:24 +0800

    > sim RTL ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-155-generic #172-Ubuntu SMP Fri Jul 7 16:10:02 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:31:24 up 84 days,  4:12,  5 users,  load average: 11.08, 11.27, 11.32

--- npc/csrc/utils.cpp
+++ npc/csrc/utils.cpp
@@ -64,7 +64,7 @@ int parse_args(int argc, char* argv[]) {
 void load_files() {
   if (!img_file) {
     Log("not detected img file, use default file %s", "tools/default-branch.bin");
-    img_file = "tools/default.bin";
+    img_file = "tools/default-branch.bin";
   }
   Log("detected img file: %s", img_file);
   init_memory(img_file);
