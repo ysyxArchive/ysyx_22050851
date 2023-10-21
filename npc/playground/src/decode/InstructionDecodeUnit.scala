f3ecf9400a27b6a0722e797683400bb79b46b314
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-21 12:40:13 +0800

    > generate verilog ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-155-generic #172-Ubuntu SMP Fri Jul 7 16:10:02 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:40:13 up 85 days, 21:20,  5 users,  load average: 12.07, 10.71, 10.41

--- npc/playground/src/decode/InstructionDecodeUnit.scala
+++ npc/playground/src/decode/InstructionDecodeUnit.scala
@@ -109,7 +109,7 @@ class InstructionDecodeUnit extends Module {
   decodeOut.bits.enable := !shouldWait
 
   // branch check
-  willTakeBranch := MuxLookup(controlDecoder.output.pcaddrsrc, false.B)(
+  willTakeBranch := !shouldWait && MuxLookup(controlDecoder.output.pcaddrsrc, false.B)(
     EnumSeq(
       PCAddrSrc.aluzero -> (src1Data === src2Data),
       PCAddrSrc.alunotneg -> (src1Data.asSInt >= src2Data.asSInt),
