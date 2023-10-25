943b33abeac6c8093d77a9316116058c543771cd
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-25 16:47:54 +0800

    > generate verilog ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-155-generic #172-Ubuntu SMP Fri Jul 7 16:10:02 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:47:54 up 90 days,  1:28,  8 users,  load average: 4.92, 4.88, 4.94

--- npc/playground/src/InstructionFetchUnit.scala
+++ npc/playground/src/InstructionFetchUnit.scala
@@ -11,7 +11,7 @@ class InstructionFetchUnit extends Module {
   val iCacheIO   = IO(Flipped(new CacheIO(64, 64)))
   val fromDecode = IO(Flipped(new DecodeBack()))
 
-  val inst           = RegInit(0x13.U(32.W))
+  val inst           = WireInit(0x13.U(32.W))
   val predictPC      = RegInit(regIO.pc)
   val lastPC         = RegInit(regIO.pc)
   val needTakeBranch = Wire(Bool())
