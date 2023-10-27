a7b73424d902032ca1ae22b6e6930f5226de3712
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-27 18:59:09 +0800

    > generate verilog ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-155-generic #172-Ubuntu SMP Fri Jul 7 16:10:02 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:59:09 up 92 days,  3:39, 11 users,  load average: 5.38, 5.32, 5.76

--- npc/playground/src/mem/MemInterface.scala
+++ npc/playground/src/mem/MemInterface.scala
@@ -126,8 +126,9 @@ class MemBurstInterface extends Module {
   mem.io.mask   := axiS.W.bits.strb
   mem.io.wdata  := axiS.W.bits.data
   mem.io.addr   := Mux(memInterfaceFSM.is(writeDataBack), readReq.addr, writeReq.addr) + (counter << 3)
-  mem.io.enable := memInterfaceFSM.is(writeDataBack) || (memInterfaceFSM.is(waitDataWrite) && axiS.W.fire)
-  dataRet       := Mux(mem.io.enable, mem.io.rdata, dataRet)
+  mem.io.enable := (memInterfaceFSM.willChangeTo(writeDataBack) && !memInterfaceFSM.willChangeTo(waitReq)) ||
+    (memInterfaceFSM.is(waitDataWrite) && axiS.W.fire)
+  dataRet := Mux(mem.io.enable, mem.io.rdata, dataRet)
 
   axiS.W.ready     := memInterfaceFSM.is(waitDataWrite)
   axiS.AW.ready    := memInterfaceFSM.is(waitReq) && !axiS.AR.valid
