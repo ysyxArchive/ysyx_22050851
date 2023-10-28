fe5aa977b4ebecad43e086eda61522dcb2766ba2
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-28 13:32:59 +0800

    > generate verilog ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-155-generic #172-Ubuntu SMP Fri Jul 7 16:10:02 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:32:59 up 92 days, 22:13, 11 users,  load average: 6.12, 5.75, 5.67

--- npc/playground/src/CPU.scala
+++ npc/playground/src/CPU.scala
@@ -7,7 +7,7 @@ class CPU extends Module {
   val enableDebug = IO(Input(Bool()))
 
   val mem         = Module(new MemBurstInterface)
-  val mem2        = Module(new MemBurstInterface)
+  // val mem2        = Module(new MemBurstInterface)
   val regs        = Module(new RegisterFile)
   val csrregs     = Module(new ControlRegisterFile)
   val blackBoxOut = Module(new BlackBoxRegs)
