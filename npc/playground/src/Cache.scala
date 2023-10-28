93165553fcefe5d54821dc08302dd7a6c63bb5b8
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-28 12:25:25 +0800

    > generate verilog ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-155-generic #172-Ubuntu SMP Fri Jul 7 16:10:02 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:25:25 up 92 days, 21:06, 11 users,  load average: 7.58, 5.87, 5.41

--- npc/playground/src/Cache.scala
+++ npc/playground/src/Cache.scala
@@ -99,7 +99,8 @@ class Cache(
   counter := PriorityMux(
     Seq(
       (counter === slotsPerLine.U) -> 0.U,
-      ((cacheFSM.is(waitRes) && axiIO.R.fire) || (cacheFSM.is(waitWRes) && axiIO.B.fire)) -> (counter + 1.U),
+      (cacheFSM.is(waitRes) && axiIO.R.fire) -> (counter + 1.U),
+      (cacheFSM.is(sendWReq) && axiIO.W.fire) -> (counter + 1.U),
       true.B -> counter
     )
   )
