e9171ab65ebc64909f43db34979c3717b6aa93b9
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-08 13:52:21 +0800

    > generate verilog ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-166-generic #183-Ubuntu SMP Mon Oct 2 11:28:33 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:52:21 up 5 days, 22:24,  8 users,  load average: 6.98, 6.85, 6.29

--- npc/playground/src/execute/Multiplier.scala
+++ npc/playground/src/execute/Multiplier.scala
@@ -91,7 +91,7 @@ class BoothModule extends Module {
     val shouldShift = Output(Bool())
   })
   io.isNeg       := io.in(2)
-  io.isWork      := io.in(0) ^ io.in(1)
+  io.isWork      := !io.in.andR && !(~io.in).andR
   io.shouldShift := (io.in(0) && io.in(1)) ^ io.in(2)
 }
 
