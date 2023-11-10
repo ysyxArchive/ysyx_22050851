6349038a61b8f49d3b60380a5d24953b56e0cdb0
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-11 00:02:36 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:02:36 up 2 days,  5:21,  0 users,  load average: 0.91, 0.44, 0.49

--- npc/playground/src/execute/Multiplier.scala
+++ npc/playground/src/execute/Multiplier.scala
@@ -7,6 +7,7 @@ import decode._
 import os.read
 import execute._
 import utils._
+import java.nio.Buffer
 
 class MultiplierIO extends Bundle {
   val mulValid     = Input(Bool()) //为高表示输入的数据有效，如果没有新的乘法输入，在乘法被接受的下一个周期要置低
@@ -127,25 +128,25 @@ class BoothMultiplier extends Module {
     Seq(
       willDone -> 0.U,
       io.flush -> 0.U,
-      (mulFSM.is(working) || mulFSM.willChangeTo(working)) -> (counter + 1.U),
+      (mulFSM.is(working) || mulFSM.willChangeTo(working)) -> (counter + 1.U)
     )
   )
 
   booth.io.in := inB >> ((Mux(io.mulw, 15.U, 31.U) - counter) * 2.U)
-   val aaaa = MuxCase(
-          0.U,
-          Seq(
-            !booth.io.isWork -> 0.U,
-            (booth.io.isWork && booth.io.isNeg) -> inANeg,
-            (booth.io.isWork && !booth.io.isNeg) -> inA
-          )
-        ) << booth.io.shouldShift
+  val valToAdd = MuxCase(
+    0.U,
+    Seq(
+      !booth.io.isWork -> 0.U,
+      (booth.io.isWork && booth.io.isNeg) -> inANeg,
+      (booth.io.isWork && !booth.io.isNeg) -> inA
+    )
+  ) << booth.io.shouldShift
   outReg := MuxCase(
     outReg,
     Seq(
       (mulFSM.is(idle) && !mulFSM.willChange()) -> 0.U,
       (mulFSM.is(working) || mulFSM.willChangeTo(working)) ->
-        ((outReg << 2.U) + (aaaa))
+        ((outReg << 2.U) + valToAdd)
     )
   )
 
@@ -153,4 +154,182 @@ class BoothMultiplier extends Module {
   io.outValid   := mulFSM.is(output)
   io.resultHigh := outReg >> 64
   io.resultLow  := outReg
+}
+
+class WallaceLayer(inputCnt: Int) extends Module {
+  val fullAdderCnt = inputCnt / 3
+  val halfAdderCnt = inputCnt % 3 / 2
+  val outSWidth    = fullAdderCnt + halfAdderCnt + inputCnt % 3 % 2
+  val outCWidth    = fullAdderCnt + halfAdderCnt
+  val io = IO(new Bundle {
+    val bits = Input(UInt(inputCnt.W))
+    val outC = Output(UInt(outCWidth.W))
+    val outS = Output(UInt(outSWidth.W))
+  })
+
+  val outS = Wire(Vec(outSWidth, Bool()))
+  val outC = Wire(Vec(outCWidth, Bool()))
+
+  val fullAdders = Seq.fill(fullAdderCnt)(Module(new FullAdder()))
+  for (i <- 0 to fullAdderCnt) {
+    val adder = fullAdders(i)
+    adder.io.inA := io.bits(i * 3)
+    adder.io.inB := io.bits(i * 3 + 1)
+    adder.io.inC := io.bits(i * 3 + 2)
+    outS(i)      := adder.io.out
+    outC(i)      := adder.io.outC
+  }
+  if (halfAdderCnt > 0) {
+    outS(fullAdderCnt) := io.bits(fullAdderCnt * 3) ^ io.bits(fullAdderCnt * 3 + 1)
+    outC(fullAdderCnt) := io.bits(fullAdderCnt * 3) && io.bits(fullAdderCnt * 3 + 1)
+  }
+  if (inputCnt % 3 % 2 > 0) {
+    outS(fullAdderCnt + halfAdderCnt) := io.bits(fullAdderCnt * 3 + halfAdderCnt * 2)
+  }
+}
+class BHMultiplier extends Module {
+  val io = IO(new MultiplierIO())
+
+  val mulFire  = io.mulValid && io.mulReady
+  val willDone = (!io.mulw && true.B) || (io.mulw && true.B)
+
+  val idle :: working :: output :: others = Enum(3)
+  val mulFSM = new FSM(
+    idle,
+    List(
+      (idle, !io.flush && mulFire, working),
+      (working, willDone, output),
+      (working, io.flush, idle),
+      (output, true.B, idle)
+    )
+  )
+
+  val inA    = io.multiplicand
+  val inB    = Cat(io.multiplier, 0.U(1.W))
+  val inANeg = Utils.signedReverse(inA)
+
+  val booths = Seq.fill(32)(Module(new BoothModule()))
+
+  val boothWork  = Wire(Vec(32, Bool()))
+  val boothNeg   = Wire(Vec(32, Bool()))
+  val boothShift = Wire(Vec(32, Bool()))
+  val addBuffer  = Wire(Vec(32, UInt(65.W)))
+
+  for (i <- 0 to 31) {
+    booths(i).io.in := inB(i * 2 + 2, i * 2)
+    val out = booths(i).io
+    boothWork(i)  := Mux(mulFSM.is(idle), out.isWork, boothWork(i))
+    boothNeg(i)   := Mux(mulFSM.is(idle), out.isNeg, boothNeg(i))
+    boothShift(i) := Mux(mulFSM.is(idle), out.shouldShift, boothShift(i))
+    addBuffer(i) := Mux(
+      mulFSM.is(idle),
+      Mux(out.isWork, Mux(out.isNeg, inANeg, inA), 0.U) << out.shouldShift,
+      addBuffer(i)
+    )
+  }
+  val wallaceLayer1 = Seq.fill(65)(Module(new WallaceLayer(32)))
+  val sBuffer1      = Reg(Vec(65, UInt(11.W)))
+  val cBuffer1      = Reg(Vec(65, UInt(11.W)))
+  for (i <- 0 to 65) {
+    wallaceLayer1(i).io.bits := VecInit(addBuffer.map(v => v(i))).asUInt
+    sBuffer1(i)              := wallaceLayer1(i).io.outS
+    cBuffer1(i)              := wallaceLayer1(i).io.outC
+  }
+  val wallaceLayer2 = Seq.fill(65)(Module(new WallaceLayer(22)))
+  val sBuffer2      = Reg(Vec(65, UInt(8.W)))
+  val cBuffer2      = Reg(Vec(65, UInt(7.W)))
+  for (i <- 0 to 65) {
+    if (i == 0) {
+      wallaceLayer2(i).io.bits := sBuffer1(i)
+    } else {
+      wallaceLayer2(i).io.bits := Cat(cBuffer1(i - 1), sBuffer1(i))
+    }
+    sBuffer2(i) := wallaceLayer2(i).io.outS
+    cBuffer2(i) := wallaceLayer2(i).io.outC
+  }
+
+  val wallaceLayer3 = Seq.fill(65)(Module(new WallaceLayer(16)))
+  val sBuffer3      = Reg(Vec(65, UInt(6.W)))
+  val cBuffer3      = Reg(Vec(65, UInt(5.W)))
+  for (i <- 0 to 65) {
+    if (i == 0) {
+      wallaceLayer3(i).io.bits := sBuffer2(i)
+    } else {
+      wallaceLayer3(i).io.bits := Cat(cBuffer2(i - 1), sBuffer2(i))
+    }
+    sBuffer3(i) := wallaceLayer3(i).io.outS
+    cBuffer3(i) := wallaceLayer3(i).io.outC
+  }
+
+  val wallaceLayer4 = Seq.fill(65)(Module(new WallaceLayer(11)))
+  val sBuffer4      = Reg(Vec(65, UInt(4.W)))
+  val cBuffer4      = Reg(Vec(65, UInt(4.W)))
+  for (i <- 0 to 65) {
+    if (i == 0) {
+      wallaceLayer4(i).io.bits := sBuffer3(i)
+    } else {
+      wallaceLayer4(i).io.bits := Cat(cBuffer3(i - 1), sBuffer3(i))
+    }
+    sBuffer4(i) := wallaceLayer4(i).io.outS
+    cBuffer4(i) := wallaceLayer4(i).io.outC
+  }
+
+  val wallaceLayer5 = Seq.fill(65)(Module(new WallaceLayer(8)))
+  val sBuffer5      = Reg(Vec(65, UInt(3.W)))
+  val cBuffer5      = Reg(Vec(65, UInt(3.W)))
+  for (i <- 0 to 65) {
+    if (i == 0) {
+      wallaceLayer5(i).io.bits := sBuffer4(i)
+    } else {
+      wallaceLayer5(i).io.bits := Cat(cBuffer4(i - 1), sBuffer4(i))
+    }
+    sBuffer5(i) := wallaceLayer5(i).io.outS
+    cBuffer5(i) := wallaceLayer5(i).io.outC
+  }
+
+  val wallaceLayer6 = Seq.fill(65)(Module(new WallaceLayer(6)))
+  val sBuffer6      = Reg(Vec(65, UInt(2.W)))
+  val cBuffer6      = Reg(Vec(65, UInt(2.W)))
+  for (i <- 0 to 65) {
+    if (i == 0) {
+      wallaceLayer6(i).io.bits := sBuffer5(i)
+    } else {
+      wallaceLayer6(i).io.bits := Cat(cBuffer5(i - 1), sBuffer5(i))
+    }
+    sBuffer6(i) := wallaceLayer6(i).io.outS
+    cBuffer6(i) := wallaceLayer6(i).io.outC
+  }
+
+  val wallaceLayer7 = Seq.fill(65)(Module(new WallaceLayer(4)))
+  val sBuffer7      = Reg(Vec(65, UInt(2.W)))
+  val cBuffer7      = Reg(Vec(65, UInt(1.W)))
+  for (i <- 0 to 65) {
+    if (i == 0) {
+      wallaceLayer7(i).io.bits := sBuffer6(i)
+    } else {
+      wallaceLayer7(i).io.bits := Cat(cBuffer6(i - 1), sBuffer6(i))
+    }
+    sBuffer7(i) := wallaceLayer7(i).io.outS
+    cBuffer7(i) := wallaceLayer7(i).io.outC
+  }
+
+  val wallaceLayer8 = Seq.fill(65)(Module(new WallaceLayer(3)))
+  val sBuffer8      = Reg(Vec(65, UInt(1.W)))
+  val cBuffer8      = Reg(Vec(65, UInt(1.W)))
+  for (i <- 0 to 65) {
+    if (i == 0) {
+      wallaceLayer8(i).io.bits := sBuffer6(i)
+    } else {
+      wallaceLayer8(i).io.bits := Cat(cBuffer7(i - 1), sBuffer7(i))
+    }
+    sBuffer8(i) := wallaceLayer8(i).io.outS
+    cBuffer8(i) := wallaceLayer8(i).io.outC
+  }
+
+  val out = sBuffer8.asUInt + cBuffer8.asUInt
+
+  io.mulReady   := mulFSM.is(idle) && !io.flush
+  io.outValid   := mulFSM.is(output)
+  io.resultHigh := out >> 64
+  io.resultLow  := out
 }
