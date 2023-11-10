f43daed792132fa153dfa74427df97501a03db41
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-11 00:14:05 +0800

    > generate verilog ysyx_22050851 李东哲 Linux daa3082d5276 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:14:05 up 2 days,  5:32,  0 users,  load average: 2.24, 1.29, 0.92

--- npc/playground/src/execute/Multiplier.scala
+++ npc/playground/src/execute/Multiplier.scala
@@ -186,8 +186,8 @@ class WallaceLayer(inputCnt: Int) extends Module {
   if (inputCnt % 3 % 2 > 0) {
     outS(fullAdderCnt + halfAdderCnt) := io.bits(fullAdderCnt * 3 + halfAdderCnt * 2)
   }
-  io.outC := outC
-  io.outS := outS
+  io.outC := outC.asUInt
+  io.outS := outS.asUInt
 }
 class BHMultiplier extends Module {
   val io = IO(new MultiplierIO())
