457b5bfc5a1dba5e992e97577a955a4a72c4fa60
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-13 15:53:31 +0800

    > generate verilog ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-166-generic #183-Ubuntu SMP Mon Oct 2 11:28:33 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:53:31 up 11 days, 26 min, 11 users,  load average: 0.21, 0.43, 0.52

--- npc/playground/src/execute/Multiplier.scala
+++ npc/playground/src/execute/Multiplier.scala
@@ -135,7 +135,6 @@ class BoothMultiplier extends Module {
   val valToAdd = MuxCase(
     0.U,
     Seq(
-      !booth.io.isWork -> 0.U,
       (booth.io.isWork && booth.io.isNeg) -> inANeg,
       (booth.io.isWork && !booth.io.isNeg) -> inA
     )
