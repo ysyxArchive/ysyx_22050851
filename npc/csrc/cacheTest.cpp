14163307cacee1ca40e61ac66a4945d2490fdbfa
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-17 15:53:23 +0800

    > sim RTL ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-166-generic #183-Ubuntu SMP Mon Oct 2 11:28:33 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:53:23 up 15 days, 26 min,  6 users,  load average: 28.89, 25.46, 24.98

--- npc/csrc/cacheTest.cpp
+++ npc/csrc/cacheTest.cpp
@@ -1,5 +1,34 @@
 #include "common.h"
 
-void check_cache(){
-    // top->iCacheInfo_cacheStatus_3_3_valid
+// void cache_
+
+bool* dcache_valid = NULL;
+bool* dcache_dirty = NULL;
+bool* icache_valid = NULL;
+bool* icache_dirty = NULL;
+
+extern "C" void set_cacheinfo_ptr(const char* name, const svOpenArrayHandle d,
+                                  const svOpenArrayHandle v) {
+  if (strcmp(name, "iCache") == 0) {
+    icache_valid = (bool*)v;
+    icache_dirty = (bool*)d;
+  } else if (strcmp(name, "dCache") == 0) {
+    dcache_valid = (bool*)v;
+    dcache_dirty = (bool*)d;
+  } else {
+    panic("unkonown cache name %s", name);
+  }
+}
+
+// ("""import "DPI-C" function void cache_change(input string name);
+//     import "DPI-C" function void set_cacheinfo_ptr(input string name, input
+//     logic [63:0] d [], input logic [63:0] v []);
+void cache_change(const char* name) {
+  if (strcmp(name, "iCache") == 0) {
+    Log("icache change");
+  } else if (strcmp(name, "dCache") == 0) {
+    Log("dcache change");
+  } else {
+    panic("unkonown cache name %s", name);
+  }
 }
