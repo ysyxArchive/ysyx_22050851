ae68b27bfe890240cb76e630f984a9fc39bbb97d
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-17 17:13:59 +0800

    > sim RTL ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-166-generic #183-Ubuntu SMP Mon Oct 2 11:28:33 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:13:59 up 15 days,  1:46,  6 users,  load average: 30.55, 27.75, 25.57

--- npc/csrc/cacheTest.cpp
+++ npc/csrc/cacheTest.cpp
@@ -7,24 +7,22 @@ const svLogicVecVal* dcache_dirty = NULL;
 const svLogicVecVal* icache_valid = NULL;
 const svLogicVecVal* icache_dirty = NULL;
 
-extern "C" void set_cacheinfo_ptr(const char* name, const svLogicVecVal* d,
+extern "C" void set_cacheinfo_ptr(svLogic isDCache, const svLogicVecVal* d,
                                   const svLogicVecVal* v) {
-  if (strcmp(name, "icache") == 0) {
+  if (!isDCache) {
     icache_valid = v;
     icache_dirty = d;
-  } else if (strcmp(name, "dcache") == 0) {
+  } else {
     dcache_valid = v;
     dcache_dirty = d;
-  } else {
-    panic("unkonown cache name %s", name);
   }
 }
 
 // ("""import "DPI-C" function void cache_change(input string name);
 //     import "DPI-C" function void set_cacheinfo_ptr(input string name, input
 //     logic [63:0] d [], input logic [63:0] v []);
-void cache_change(const char* name) {
-  if (strcmp(name, "icache") == 0) {
+void cache_change(svLogic isDCache) {
+  if (!isDCache) {
     for (int i = 0; i < 4; i++) {
       for (int j = 0; j < 4; j++) {
         printf("%x", icache_valid[i * 4 + j].aval);
@@ -37,9 +35,7 @@ void cache_change(const char* name) {
       }
     }
     printf("\n");
-  } else if (strcmp(name, "dcache") == 0) {
-    Log("dcache change");
   } else {
-    panic("unkonown cache name %s", name);
+    Log("dcache change");
   }
 }
