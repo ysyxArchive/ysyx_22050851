3533a2c8afc8817d3cd5a833cec49fa84417888d
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-17 22:07:03 +0800

    > sim RTL ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-166-generic #183-Ubuntu SMP Mon Oct 2 11:28:33 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:07:03 up 15 days,  6:39,  7 users,  load average: 19.40, 22.29, 22.23

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -103,7 +103,7 @@ void init_proc() {
   char* args[] = {"/bin/pal", NULL};
   char* envp[] = {NULL};
   // context_kload(getPCB(), hello_fun, args);
-  context_uload(getPCB(), "/bin/pal", args, envp);
+  context_uload(getPCB(), "/bin/nslider", args, envp);
   switch_boot_pcb();
 
 }
