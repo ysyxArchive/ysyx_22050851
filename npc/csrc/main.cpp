a9645649de0e25297daf63f6ba767e218e8fbd11
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-17 21:57:56 +0800

    > sim RTL ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-166-generic #183-Ubuntu SMP Mon Oct 2 11:28:33 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:57:56 up 15 days,  6:30,  7 users,  load average: 22.61, 22.00, 21.65

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -139,8 +139,7 @@ int main(int argc, char* argv[]) {
   auto start = std::chrono::high_resolution_clock::now();
   while (!is_halt) {
     one_step();
-    if (inst_count % (int)1e5 == 0) {
-      Log("%ld", inst_count);
+    if (cycle_count % (int)1e5 == 0) {
       auto end = std::chrono::high_resolution_clock::now();
       auto dur =
           std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
