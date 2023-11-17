1a95e28bdaadc00a5d2170876542c6fee646afaa
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-17 19:12:46 +0800

    > generate verilog ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-166-generic #183-Ubuntu SMP Mon Oct 2 11:28:33 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:12:46 up 15 days,  3:45,  6 users,  load average: 21.98, 24.28, 24.15

--- npc/playground/src/cache/BlackBoxCache.scala
+++ npc/playground/src/cache/BlackBoxCache.scala
@@ -5,20 +5,23 @@ class CacheDebugIO(
   val wayCnt:    Int = 4,
   val groupSize: Int = 4)
     extends Bundle {
-  val cacheStatus =
-    Vec(
-      wayCnt,
-      Vec(
-        groupSize,
-        new Bundle {
-          val valid = Bool()
-          val dirty = Bool()
-        }
-      )
-    )
+  // val cacheStatus =
+  //   Vec(
+  //     wayCnt,
+  //     Vec(
+  //       groupSize,
+  //       new Bundle {
+  //         val valid = Bool()
+  //         val dirty = Bool()
+  //       }
+  //     )
+  //   )
   val changed  = Bool()
   val clock    = Clock()
   val isDCache = Bool()
+  val reqValid = Bool()
+  val isHit    = Bool()
+  val reqWrite = Bool()
 }
 
 class BlackBoxCache(wayCnt: Int = 4, groupSize: Int = 4) extends BlackBox with HasBlackBoxInline {
@@ -26,34 +29,40 @@ class BlackBoxCache(wayCnt: Int = 4, groupSize: Int = 4) extends BlackBox with H
   setInline(
     "BlackBoxCache.v",
     (s"""import "DPI-C" function void cache_change(input logic isDCache, input logic d [0:15], input logic v [0:15]);
+    import "DPI-C" function void cache_req(input logic isDCache, input logic isHit, input logic reqWrite);
        |module BlackBoxCache (
        |  input isDCache,
        |  input changed,
+       |  input reqValid,
+       |  input isHit,
+       |  input reqWrite,
+       |  input clock
        """ +
-      Seq
-        .tabulate(wayCnt)(i =>
-          Seq
-            .tabulate(groupSize)(j => s"| input cacheStatus_${i}_${j}_valid,\n| input cacheStatus_${i}_${j}_dirty,\n")
-            .reduce(_ ++ _)
-        )
-        .reduce(_ ++ _) +
-      s"""|  input clock
+      // Seq
+      //   .tabulate(wayCnt)(i =>
+      //     Seq
+      //       .tabulate(groupSize)(j => s"| input cacheStatus_${i}_${j}_valid,\n| input cacheStatus_${i}_${j}_dirty,\n")
+      //       .reduce(_ ++ _)
+      //   )
+      //   .reduce(_ ++ _) +
+      s"""|  
           |);
-          |  wire valid [0:${wayCnt * groupSize - 1}];
-          |  wire dirty [0:${wayCnt * groupSize - 1}];
-      """ + Seq
-      .tabulate(wayCnt)(i =>
-        Seq
-          .tabulate(groupSize)(j =>
-            s"| assign valid[${i * groupSize + j}] = cacheStatus_${i}_${j}_valid;\n| assign dirty[${i * groupSize + j}] = cacheStatus_${i}_${j}_dirty;\n"
-          )
-          .reduce(_ ++ _)
-      )
-      .reduce(_ ++ _) +
+      """ +
+      // Seq
+      // .tabulate(wayCnt)(i =>
+      //   Seq
+      //     .tabulate(groupSize)(j =>
+      //       s"| assign valid[${i * groupSize + j}] = cacheStatus_${i}_${j}_valid;\n| assign dirty[${i * groupSize + j}] = cacheStatus_${i}_${j}_dirty;\n"
+      //     )
+      //     .reduce(_ ++ _)
+      // )
+      // .reduce(_ ++ _) +
       s"""  
          |  always @(posedge clock) begin
          |    if(changed) cache_change(isDCache, dirty,valid);
+         |    if(reqValid) cache_req(isDCache, isHit, reqWrite);
          |  end
+         |    
          |endmodule""").stripMargin
   )
 }
