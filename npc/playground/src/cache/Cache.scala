cff67edeae725e41e3f9e7dd241b410d5eeab1b0
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-21 15:43:52 +0800

    > generate verilog ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-166-generic #183-Ubuntu SMP Mon Oct 2 11:28:33 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:43:52 up 19 days, 16 min,  8 users,  load average: 18.09, 17.69, 17.13

--- npc/playground/src/cache/Cache.scala
+++ npc/playground/src/cache/Cache.scala
@@ -68,6 +68,16 @@ class Cache(
 
   val counter    = RegInit(0.U(log2Ceil(slotsPerLine).W))
   val directData = Reg(UInt(dataWidth.W))
+  
+  val tag    = Mux(cacheFSM.is(idle), io.addr, addr)(addrWidth - 1, tagOffset)
+  val index  = Mux(cacheFSM.is(idle), io.addr, addr)(tagOffset - 1, indexOffset)
+  val offset = Mux(cacheFSM.is(idle), io.addr, addr)(indexOffset - 1, 0)
+  
+  val replaceIndex = RegInit(0.U(log2Ceil(groupSize).W))
+
+  val wayValid    = cacheMem(index).map(line => line.valid && line.tag === tag)
+  val targetIndex = Mux1H(wayValid, Seq.tabulate(groupSize)(index => index.U))
+  val data        = cacheMem(index)(targetIndex).data
 
   val shoudDirectRW = io.addr > 0xa0000000L.U
   val cacheFSM = new FSM(
@@ -124,15 +134,6 @@ class Cache(
 
   isRead := Mux(cacheFSM.is(idle), io.readReq.fire, isRead)
 
-  val replaceIndex = RegInit(0.U(log2Ceil(groupSize).W))
-
-  val tag    = Mux(cacheFSM.is(idle), io.addr, addr)(addrWidth - 1, tagOffset)
-  val index  = Mux(cacheFSM.is(idle), io.addr, addr)(tagOffset - 1, indexOffset)
-  val offset = Mux(cacheFSM.is(idle), io.addr, addr)(indexOffset - 1, 0)
-
-  val wayValid    = cacheMem(index).map(line => line.valid && line.tag === tag)
-  val targetIndex = Mux1H(wayValid, Seq.tabulate(groupSize)(index => index.U))
-  val data        = cacheMem(index)(targetIndex).data
 
   hit     := wayValid.reduce(_ || _)
   isDirty := cacheMem(index)(replaceIndex).dirty
