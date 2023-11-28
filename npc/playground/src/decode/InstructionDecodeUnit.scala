980eed036e29e5c0b42b4cf05342424ae0a29f40
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-28 16:41:35 +0800

    > generate verilog ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-167-generic #184-Ubuntu SMP Tue Oct 31 09:21:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:41:35 up 1 day, 18:10,  6 users,  load average: 17.34, 24.74, 25.55

--- npc/playground/src/decode/InstructionDecodeUnit.scala
+++ npc/playground/src/decode/InstructionDecodeUnit.scala
@@ -113,10 +113,6 @@ class InstructionDecodeUnit extends Module {
     Utils.cast(src2RawData, 32, 64),
     src2RawData
   )
-  decodeOut.bits.data.src1Data      := src1Data
-  decodeOut.bits.data.src1DataValid := rs1 === 0.U || !regVec.contains(rs1)
-  decodeOut.bits.data.src2Data      := src2Data
-  decodeOut.bits.data.src2DataValid := rs2 === 0.U || !regVec.contains(rs2)
 
   // RAW check
   val regVec = VecInit(Seq(fromExe, fromMemu, fromWbu).map(bundle => Mux(bundle.dataValid, 0.U, bundle.regIndex)))
@@ -155,9 +151,13 @@ class InstructionDecodeUnit extends Module {
     )
   )
 
-  decodeBack.valid          := dataValid && !shouldWait
-  decodeBack.willTakeBranch := willTakeBranch
-  decodeBack.branchPc       := branchPc
+  decodeBack.valid                  := dataValid && !shouldWait
+  decodeBack.willTakeBranch         := willTakeBranch
+  decodeBack.branchPc               := branchPc
+  decodeOut.bits.data.src1Data      := src1Data
+  decodeOut.bits.data.src1DataValid := rs1 === 0.U || !regVec.contains(rs1)
+  decodeOut.bits.data.src2Data      := src2Data
+  decodeOut.bits.data.src2DataValid := rs2 === 0.U || !regVec.contains(rs2)
 
   decodeOut.bits.data.dnpc     := Mux(shouldWait, decodeInReg.pc, Mux(willTakeBranch, branchPc, decodeInReg.snpc))
   decodeOut.bits.toDecodeValid := controlDecoder.output.regwritemux === RegWriteMux.snpc.asUInt
