4377ff25e59bedcb0b438090e36d7d149747466e
tracer-ysyx2204 <tracer@ysyx.org>
2023-12-04 14:51:47 +0800

    > compile NEMU ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-167-generic #184-Ubuntu SMP Tue Oct 31 09:21:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:51:47 up 7 days, 16:20, 11 users,  load average: 1.76, 1.72, 1.82

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -22,8 +22,8 @@ CXX := g++
 endif
 LD := $(CXX)
 INCLUDES = $(addprefix -I, $(INC_PATH))
-CFLAGS  := -O3 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
-LDFLAGS := -O3 $(LDFLAGS)
+CFLAGS  := -O0 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
+LDFLAGS := -O0 $(LDFLAGS)
 
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
