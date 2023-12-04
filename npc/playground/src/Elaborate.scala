3bd1bed5c5eb5c14f2133b546c10bf1bf24d968a
tracer-ysyx2204 <tracer@ysyx.org>
2023-12-04 18:42:49 +0800

    > generate verilog ysyx_22050851 李东哲 Linux xilinx-Precision-7920-Tower 5.4.0-167-generic #184-Ubuntu SMP Tue Oct 31 09:21:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:42:49 up 7 days, 20:11, 11 users,  load average: 31.08, 29.06, 23.31

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -2,10 +2,14 @@ import circt.stage._
 
 object Elaborate extends App {
   println(args.mkString(", "))
-  var filteredArgs = args.clone()
-  val isDebug      = args.contains("--debug")
+  var filteredArgs     = args.clone()
+  val isDebug          = args.contains("--debug")
+  val shouldRemoveDPIC = args.contains("--rm-dpic")
   if (isDebug) {
     filteredArgs = args.filter(_ != "--debug")
+  }
+  if (shouldRemoveDPIC) {
+    filteredArgs = args.filter(_ != "--rm-dpic")
   }
   def top       = new CPU(isDebug)
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
